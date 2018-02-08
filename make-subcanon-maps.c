b4745b932fba70fc25cfb2833f2e1e0fd90e34f7
Brennan Tracy <brennantracy4@gmail.com>
2018-02-07 23:07:31 -0800

    add make-subcanon-maps

---
+++ make-subcanon-maps.c
@@ -0,0 +1,218 @@
+#include <stdio.h>
+#include <sys/file.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include "misc.h"
+#include "tinygraph.h"
+#include "blant.h"
+
+
+//needed for blant stuff
+#define maxBk (1 << (maxK*(maxK-1)/2)) // maximum number of entries in the canon_map
+#define CANON_DIR "canon_maps"
+
+// The following is the most compact way to store the permutation between a non-canonical and its canonical representative,
+// when k=8: there are 8 entries, and each entry is a integer from 0 to 7, which requires 3 bits. 8*3=24 bits total.
+// For simplicity we use the same 3 bits per entry, and assume 8 entries, even for k<8.  It wastes memory for k<4, but
+// makes the coding much simpler.
+typedef unsigned char kperm[3]; // The 24 bits are stored in 3 unsigned chars.
+
+static unsigned int Bk, _k; // _k is the global variable storing k; Bk=actual number of entries in the canon_map for given k.
+
+// Here's where we're lazy on saving memory, and we could do better.  We're going to allocate a static array
+// that is big enough for the 256 million permutations from non-canonicals to canonicals for k=8, even if k<8.
+// So we're allocating 256MBx3=768MB even if we need much less.  I figure anything less than 1GB isn't a big deal
+// these days. It needs to be aligned to a page boundary since we're going to mmap the binary file into this array.
+static kperm Permutations[maxBk] __attribute__ ((aligned (8192)));
+// Here's the actual mapping from non-canonical to canonical, same argument as above wasting memory, and also mmap'd.
+// So here we are allocating 256MB x sizeof(short int) = 512MB.
+// Grand total statically allocated memory is exactly 1.25GB.
+static short int K[maxBk] __attribute__ ((aligned (8192)));
+
+//Needed for BuildGraph
+static TINY_GRAPH *G;
+static int k;
+
+#define MMAP 1 
+// Try to mmap, and if it fails, just slurp in the file (sigh, Windoze)
+void *Mmap(void *p, size_t n, int fd)
+{
+#if MMAP
+    void *newPointer = mmap(p, n, PROT_READ, MAP_PRIVATE|MAP_FIXED, fd, 0);
+    if(newPointer == MAP_FAILED)
+#endif
+    {
+#if !__WIN32__ && !__CYGWIN__ // it will always fail on Windoze so don't bother with a warning
+	Warning("mmap failed");
+#endif
+	if(read(fd, p, n) != n)
+	    Fatal("cannot mmap, or cannot read the file, or both");
+    }
+    return p;
+}
+
+static void BuildGraph(int Gint)
+{
+    int i, j, bitPos=0;
+    int Gint2 = Gint;  // Gint2 has bits nuked as they're used, so when it's zero we can stop.
+    TinyGraphEdgesAllDelete(G);
+#if LOWER_TRIANGLE
+    for(i=k-1;i>0;i--)
+    {
+	for(j=i-1;j>=0;j--)
+#else	// UPPER_TRIANGLE
+    for(i=k-2;i>=0;i--)
+    {
+	for(j=k-1;j>i;j--)
+#endif
+	{
+	    if(!Gint2) break;
+	    int bit = (1 << bitPos);
+	    if(Gint & bit)
+		TinyGraphConnect(G,i,j);
+	    Gint2 &= ~bit;
+	    bitPos++;
+	}
+	if(!Gint2) break;
+    }
+}
+
+#define LOWER_TRIANGLE 1
+// Given a TINY_GRAPH and k, return the integer ID cretaed from one triangle (upper or lower) of the adjacency matrix.
+static int TinyGraph2Int(TINY_GRAPH *g, int numNodes)
+{
+    int i, j, bitPos=0, Gint = 0, bit;
+    
+#if LOWER_TRIANGLE	// Prefer lower triangle to be compatible with Ine Melckenbeeck's Jesse code.
+    for(i=numNodes-1;i>0;i--)
+    {
+        for(j=i-1;j>=0;j--)
+#else   // UPPER_TRIANGLE // this is what we used in the original faye code and paper with Adib Hasan and Po-Chien Chung.
+    for(i=numNodes-2;i>=0;i--)
+    {
+        for(j=numNodes-1;j>i;j--)
+#endif
+        {
+	    if(TinyGraphAreConnected(g,i,j))
+	    {
+		bit = (1 << bitPos);
+		Gint |= bit;
+	    }
+            bitPos++;
+        }
+    }
+    return Gint;
+}
+
+const char *byte_to_binary(int x)
+{
+    static char b[9];
+    b[0] = '\0';
+
+    int z;
+    for (z = 128; z > 0; z >>= 1)
+    {
+        strcat(b, ((x & z) == z) ? "1" : "0");
+    }
+
+    return b;
+}
+
+
+int main(int argc, char* argv[]) {
+    /*
+    read from canon list files
+    for each: 
+        build the G canonical (tinyGraph)
+        remove nodes 1 a a time. and then call tinygraph->whatcanonicalami
+            use induced function called k times. call canon map on induced .
+    */
+    int i, j;
+    k=atoi(argv[1]);
+    _k = k;
+    G = TinyGraphAlloc(k);
+
+    Bk = (1 <<(k*(k-1)/2));
+    char BUF[BUFSIZ];
+    //Create canon list for k
+    sprintf(BUF, CANON_DIR "/canon_list%d.txt", k);
+    FILE *fp_ord=fopen(BUF, "r");
+    assert(fp_ord);
+    int numCanon;
+    fscanf(fp_ord, "%d",&numCanon);
+    int canon_list[numCanon];
+    for(i=0; i<numCanon; i++) fscanf(fp_ord, "%d", &canon_list[i]);
+    fclose(fp_ord);
+
+    // //Create canon list for k-1
+    // sprintf(BUF, CANON_DIR "/canon_list%d.txt", k-1);
+    // FILE *fp_ord=fopen(BUF, "r");
+    // assert(fp_ord);
+    // int numCanon2;
+    // fscanf(fp_ord, "%d",&numCanon);
+    // int canon_list2[numCanon];
+    // for(i=0; i<numCanon; i++) fscanf(fp_ord, "%d", &canon_list[i]);
+    // fclose(fp_ord);
+
+    //Getting and filling canon maps
+    char perm[maxK+1];
+    sprintf(BUF, CANON_DIR "/canon_map%d.bin", k);
+    int Kfd = open(BUF, 0*O_RDONLY);
+    sprintf(BUF, CANON_DIR "/perm_map%d.bin", k);
+    int pfd = open(BUF, 0*O_RDONLY);
+    short int *Kf = Mmap(K, Bk*sizeof(K[0]), Kfd);
+    kperm *Pf = Mmap(Permutations, Bk*sizeof(Permutations[0]), pfd);
+    assert(Kf == K);
+    assert(Pf == Permutations);
+
+    //How to access element from canonmap
+    // TINY_GRAPH *g = TinyGraphAlloc(k-1);
+    // int Gint = TinyGraph2Int(g,k);
+    // int j;
+	// for(j=0;j<k;j++) perm[j]=0;
+	// ExtractPerm(perm, Gint);
+	// //printf("K[%d]=%d [%d];", Gint, K[Gint], canon_list[K[Gint]]);
+	// printf("%d", K[Gint]); // Note this is the ordinal of the canonical, not its bit representation
+	// for(j=0;j<k;j++) printf(" %d", Varray[(unsigned)perm[j]]);
+	// puts("");
+
+    /*
+        For every canonical in canonical_list
+            create tinygraph
+            print canonical
+            
+            For every induced subgraph of size k-1
+                get canonical from k-1
+                print decimal for canonical
+            print newline
+
+    */
+    TINY_GRAPH *g;
+    TSET induceTSET;
+    int Gint = 0;
+    for (int i = 0; i < numCanon; i++) {
+        int canonical = canon_list[i];
+        BuildGraph(canonical);
+        printf("%d ", canonical); 
+
+        //Reset induceTSET
+        for (int j = 0; j < k; j++) {
+            //g = TinyGraphCopy(g, G);              
+
+            //get induced, print canonical + " "
+            induceTSET = TSET_NULLSET;
+            for (int tsetBit = 0; tsetBit < k; tsetBit++) {
+                induceTSET <<= 1;
+                if (tsetBit != j)
+                    induceTSET |= 1;
+            }
+
+            g = TinyGraphInduced(NULL, G, induceTSET);
+            Gint = TinyGraph2Int(g, k);
+            printf("%d ", K[Gint]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
