3e09e7926d02573b4febdf3ea159b864e917afb6
Brennan Tracy <brennantracy4@gmail.com>
2018-02-07 23:51:07 -0800

    remove debug code

--- make-subcanon-maps.c
+++ make-subcanon-maps.c
@@ -106,21 +106,6 @@ static int TinyGraph2Int(TINY_GRAPH *g, int numNodes)
     return Gint;
 }
 
-const char *byte_to_binary(int x)
-{
-    static char b[9];
-    b[0] = '\0';
-
-    int z;
-    for (z = 128; z > 0; z >>= 1)
-    {
-        strcat(b, ((x & z) == z) ? "1" : "0");
-    }
-
-    return b;
-}
-
-
 int main(int argc, char* argv[]) {
     /*
     read from canon list files
