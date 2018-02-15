e9ae43a8b5fe9a920a5cd3a64947b23f32dac363
Brennan Tracy <brennantracy4@gmail.com>
2018-02-15 15:37:42 -0800

    remove conflicts with k macro and libwayne

--- Makefile
+++ Makefile
@@ -27,7 +27,7 @@ canon_map7 canon_map8:
 
 canon_maps/canon_map6.txt: blant.h make-canon-maps
 	mkdir -p canon_maps
-	for i in 3 4 5 6; do ./make-canon-maps $$i 1 0 | cut -f2- | tee canon_maps/canon_map$$i.txt | awk '!seen[$$1]{seen[$$1]=1;map[n++]=$$1}END{print n;for(i=0;i<n;i++)printf "%d ", map[i]; print ""}' | tee canon_maps/canon_list$$i.txt | awk 'NR==2{for(i=1;i<=NF;i++) print i-1, $$i}' > canon_maps/canon-ordinal-to-signature$$i.txt; gcc -O2 "-Dk=$$i" "-DkString=\"$$i\"" -o create-bin-data create-bin-data.c $(LIBWAYNE); ./create-bin-data; done
+	for i in 3 4 5 6; do ./make-canon-maps $$i 1 0 | cut -f2- | tee canon_maps/canon_map$$i.txt | awk '!seen[$$1]{seen[$$1]=1;map[n++]=$$1}END{print n;for(i=0;i<n;i++)printf "%d ", map[i]; print ""}' | tee canon_maps/canon_list$$i.txt | awk 'NR==2{for(i=1;i<=NF;i++) print i-1, $$i}' > canon_maps/canon-ordinal-to-signature$$i.txt; gcc -O2 "-Dkk=$$i" "-DkString=\"$$i\"" -o create-bin-data create-bin-data.c $(LIBWAYNE); ./create-bin-data; done
 	/bin/rm -f create-bin-data # it's not useful after this
 
 canon_maps/canon_map7.txt: blant.h make-canon-maps
--- create-bin-data.c
+++ create-bin-data.c
@@ -7,12 +7,12 @@
 ** This file is intended to take the canon_maps/*.txt files and create binary
 ** versions in whatever format your local machine uses; these binary versions
 ** then allow BLANT/faye to work much faster at sampling.  NOTE however
-** that if we ever use a k greater than 8 (ie., graphlets larger than 8)
+** that if we ever use a kk greater than 8 (ie., graphlets larger than 8)
 ** then this code will need to be rewritten.  Right now it assumes the
 ** very nice, compact representation that graphlets of maximum 8 nodes
 ** allows: exactly 3 bits required per node to specify the permutation,
 ** times 8 nodes, is exactly 24 bits to specify a full permutation from a
-** non-canonical to a canonical.  We also use this representation for k<8,
+** non-canonical to a canonical.  We also use this representation for kk<8,
 ** even though it's not as compact as possible, it's good enough.  9 bits
 ** will make it more messy: you'd need 4 bits to specify a permutation,
 ** times 9 nodes gives 36 bits, which requires 5 bytes (4.5 actually)
@@ -25,30 +25,30 @@
 ** the known universe.
 */
 
-#ifndef k
-#error define k as an integer between 3 and 8
+#ifndef kk
+#error define kk as an integer between 3 and 8
 #endif
 #ifndef kString
-#error must define kString as an string between "3" and "8" corresponding to value of k above
+#error must define kString as an string between "3" and "8" corresponding to value of kk above
 #endif
 
 typedef unsigned char kperm[3]; // 3 bits per permutation, max 8 permutations = 24 bits
-#define Bk (1 <<(k*(k-1)/2))
-short int K[Bk]; // does not NEED to be unsigned, so leave it signed, since for k<=8 max_Bk is 12346 < 32657
+#define Bk (1 <<(kk*(kk-1)/2))
+short int K[Bk]; // does not NEED to be unsigned, so leave it signed, since for kk<=8 max_Bk is 12346 < 32657
 kperm Permutations[Bk];
 
-void ExtractPerm(char perm[k], int i) // you provide a permutation array, we fill it with permutation i
+void ExtractPerm(char perm[kk], int i) // you provide a permutation array, we fill it with permutation i
 {
     int j, i32 = 0;
     for(j=0;j<3;j++) i32 |= (Permutations[i][j] << j*8);
-    for(j=0;j<k;j++)
+    for(j=0;j<kk;j++)
 	perm[j] = (i32 >> 3*j) & 7;
 }
 
-void EncodePerm(kperm *p, char perm[k]) // you provide 3 bytes of storage and a permutation.
+void EncodePerm(kperm *p, char perm[kk]) // you provide 3 bytes of storage and a permutation.
 {
     int j, i32=0;
-    for(j=0;j<k;j++)
+    for(j=0;j<kk;j++)
 	i32 |= (perm[j] << (3*j));
     for(j=0;j<3;j++) (*p)[j] = ((i32 >> j*8) & 255);
 }
@@ -88,14 +88,14 @@ int main(int argc, char *argv[])
 	assert(numRead == 2 || numRead == 3);
 	ordinal=canon2ordinal(numCanon, canon_list, canonical);
 	K[line]=ordinal;
-	for(i=0;i<k;i++)perm[i] -= '0';
+	for(i=0;i<kk;i++)perm[i] -= '0';
 	EncodePerm(&Permutations[line], perm);
 #if 0  // output sanity checking info?
 	if(numRead == 3) fputs(buf,stdout);
-	for(i=0;i<k;i++)perm[i]=0;
+	for(i=0;i<kk;i++)perm[i]=0;
 	ExtractPerm(perm, line);
 	printf("K[%d]=%d;", line, K[line]);
-	for(i=0;i<k;i++)printf(" %d", perm[i]);
+	for(i=0;i<kk;i++)printf(" %d", perm[i]);
 	printf("\n");
 #endif
     }
