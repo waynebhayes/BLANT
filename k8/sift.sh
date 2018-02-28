569b44fed6e9524f5e19ef90e5f67b0635fe15bd
waynebhayes <whayes@uci.edu>
2018-02-28 07:03:44 -0800

    editing sift.sh back to k=7 from k=8 upper runs

--- k8/sift.sh
+++ k8/sift.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 # Whatever you use to run things in parallel.  Maybe SGE?  Up to you. I run them in parallel on a machine with 8-64 cores.
-CORES=32
-P="parallel $CORES"
+CORES=8
+P="parallel $CORES" # pipe your list of command lines into "parallel" to run them together on the same machine.
 #P="distrib_stdin.new -f ../machines.89" # Wayne's personal SGE replacement
 
 # Choose your value of k. For testing use k=7.
 # For k=8, it's 1-2 months of single-core CPU time.
-k=8
+k=7
 
-MC=../make-canon-maps.upper
-SIFT=../canon-sift.upper # the executable used to do the sifting.
+MC=../make-canon-maps # makes the initial canon maps in batch of 512 runs.
+SIFT=../canon-sift # the executable used to do the sifting down to the final canon_map file.
 
 # You should create this directory manually and remove it when done
 TMP=/tmp/canon-sift  # use this one if you're running all on one machine
@@ -36,7 +36,7 @@ awk 'BEGIN{for(i=0;i<2;i++)printf "'$SIFT' '$k' '$TMP'/'$k'.7.%03d '$TMP'/'$k'.7
 awk 'BEGIN{for(i=0;i<1;i++)printf "'$SIFT' '$k' '$TMP'/'$k'.8.%03d '$TMP'/'$k'.8.%03d > '$TMP'/'$k'.9.%03d\n",2*i,2*i+1,i;exit}' | $P &&
 
 cut -f2- $TMP/$k.9.000 | tee canon_map$k.txt | awk '!seen[$1]{seen[$1]=1;map[n++]=$1}END{print n;for(i=0;i<n;i++)printf "%d ", map[i]; print ""}' | tee canon_list$k.txt | awk 'NR==2{for(i=1;i<=NF;i++) print i-1, $i}' > canon-ordinal-to-signature$k.txt 
-
+set +x
 echo "The hard work is now done. When you are happy, you can do the following:"
 echo "mv canon*$k.txt ../canon_maps"
 echo "(cd ..; gcc -O2 '-Dk=$k' '-DkString='\"$k\"' -o create-bin-data create-bin-data.c; ./create-bin-data)"
