5149465cf2bf7601c52cae4a4a4fa3b86dba403a
waynebhayes <whayes@uci.edu>
2018-07-29 06:52:42 -0700

    synthetic now kinda works but core dumps

--- synthetic.c
+++ synthetic.c
@@ -10,6 +10,7 @@
 #include "graph.h"
 #include "blant.h"
 #include "sets.h"
+#include "matvec.h" // for VecDiff, etc.
 
 char USAGE[] = "synthetic -k k Gtarget.el Gsynth.el blant.Gt.index blant.Gs.index\n - output is new Gsynth.el\n";
 
@@ -23,7 +24,7 @@ typedef unsigned char kperm[3]; // The 24 bits are stored in 3 unsigned chars.
 
 static unsigned int _Bk, _k; // _k is the global variable storing k; _Bk=actual number of entries in the canon_map for given k.
 static int _numCanon, _canonList[MAX_CANONICALS];
-static int _numOrbits, _orbitList[MAX_CANONICALS][maxK];
+static int _numOrbits, _orbitList[MAX_CANONICALS][maxK], _numSamples;
 
 // Here's where we're lazy on saving memory, and we could do better.  We're going to allocate a static array
 // that is big enough for the 256 million permutations from non-canonicals to canonicals for k=8, even if k<8.
@@ -53,10 +54,36 @@ void SetGlobalCanonMaps(void)
     assert(Pf == Permutations);
 }
 
+// Given the big graph G and a set of nodes in V, return the TINY_GRAPH created from the induced subgraph of V on G.
+static TINY_GRAPH *TinyGraphInducedFromGraph(TINY_GRAPH *Gv, GRAPH *G, int *Varray)
+{
+    unsigned i, j;
+    TinyGraphEdgesAllDelete(Gv);
+    for(i=0; i < Gv->n; i++) for(j=i+1; j < Gv->n; j++)
+        if(GraphAreConnected(G, Varray[i], Varray[j]))
+            TinyGraphConnect(Gv, i, j);
+    return Gv;
+}
+
+void ReBLANT(double *D, GRAPH *G, SET **samples, int **B, int v1, int v2)
+{
+    int line;
+    static TINY_GRAPH *g;
+    static int Varray[maxK];
+    if(!g) g = TinyGraphAlloc(_k);
+    
+    for(line = 0; line < _numSamples; line++) if(SetIn(samples[v1], line) && SetIn(samples[v2], line))
+    {
+	--D[B[line][0]];
+	TinyGraphInducedFromGraph(g, G, B[line]+1);
+	B[line][0] = _K[TinyGraph2Int(g, _k)];
+	++D[B[line][0]];
+    }
+}
 
 int main(int argc, char *argv[])
 {
-    int i, opt, numSamples=0, j, line;
+    int i, opt, j, line;
 
     if(argc == 1)
     {
@@ -93,10 +120,11 @@ int main(int argc, char *argv[])
     }
 
     // The distribution of graphlets (squiggly plot vectors)
-    int D[2][_numCanon];
+    // NOTE we use DOUBLE not INT so we can use VecDiff from my libwayne/matvec
+    double D[2][_numCanon], vecDiff[_numCanon];
     for(i=0; i<_numCanon; i++) D[0][i] = D[1][i] = 0;
 
-    int nSamples[2], S, **BLANT[2];
+    int nSamples[2], **BLANT[2];
     for(i=0;i<2;i++)
     {
 	char cmd[BUFSIZ];
@@ -122,48 +150,54 @@ int main(int argc, char *argv[])
 	optind++;
     }
     assert(nSamples[0] == nSamples[1]);
-    S = nSamples[0];
+    _numSamples = nSamples[0];
     for(i=0;i<2;i++)
     {
 	int testCount = 0;
 	for(j=0; j<_numCanon; j++) testCount += D[i][j];
-	assert(testCount == S);
+	assert(testCount == _numSamples);
     }
 
     SET **samples = (SET**)Malloc(G[1]->n * sizeof(SET*));
     for(i=0; i < G[1]->n; i++)
-	samples[i] = SetAlloc(S);
-    for(line = 0; line < S; line++)
+	samples[i] = SetAlloc(_numSamples);
+    for(line = 0; line < _numSamples; line++)
 	for(j=1; j<=_k; j++)
 	    SetAdd(samples[BLANT[1][line][j]], line);
 
-/*
+    // while(not done---either some number of iterations, or objective function says we're too far away)
+    while(VecLength(_numCanon, VecDiff(_numCanon, vecDiff, D[0], D[1])) > 0.1)
+    {
+	putc('.', stderr); fflush(stderr);
+	int edge = drand48() * G[1]->numEdges;
+	int u1, u2, *pV1 = &(G[1]->edgeList[2*edge]), *pV2 = &(G[1]->edgeList[2*edge+1]);
+	do {
+	    u1 = drand48()*G[1]->n;
+	    u2 = drand48()*G[1]->n;
+	} while(GraphAreConnected(G[1], u1, u2)); // find a non-edge
+	assert(GraphAreConnected(G[1], *pV1, *pV2));
+	GraphDisconnect(G[1], *pV1, *pV2); // remove edge e from Gs
+	ReBLANT(D[1], G[1], samples, BLANT[1], *pV1, *pV2);
+	GraphConnect(G[1], u1, u2);
+	ReBLANT(D[1], G[1], samples, BLANT[1], u1, u2);
 
-while(not done---either some number of iterations, or objective function says we're too far away)
-{
-    pick an edge e = (v1, v2) from synthetic network Gs; // we're going to move it elsewhere
-    do {eNew = random u1 and u2} while(IsEdge(Gs, u1,u2)); // find a non-edge
-    // delete edge e from Gs and add eNew, then update the index + distribution vectors
-    assert(GraphIsEdge(Gs, v1, v2));
-    GraphDisconnect(Gs, v1, v2); // remove edge e from Gs
-    for(L in samples[v1])
-        if(SetIn(samples[v2], L)) // both v1 and v2 are in sample L
-            decrement counter for Ds[line L's first column]
-            Recompute canonical and set the first column appropriately
-            increment counter for Ds[line L's new first column]
-        end if // we've now determined the new graphlet for line L of Gs's index after removing edge e
-    end for
-    GraphConnect(Gs, u1, u2); // add edge eNew to Gs
-    Now do the same thing for(L in samples[u1]) and if(SetIn(samples[u2], L)....
-    ..... bla bla // determined new graphlet for line L of Gs's index after adding edge eNew
-    end for
-    Compute objective function comparing distribution(Gs) to distribution(Gt)
-    // note we should do this "efficiently" using a delta rather than recomputing whole objective
-    if(we need to revert the change)
-        revert all of the above: move eNew back to e and reverse all the changes.
-    end if
-}
-output edge list of Gs
-*/
-
+	double newDiff[_numCanon];
+	// note we should do this "efficiently" using a delta rather than recomputing whole objective
+	VecDiff(_numCanon, newDiff, D[0], D[1]);
+	// Hill Climbing for now
+	if(VecLength(_numCanon, newDiff) < VecLength(_numCanon, vecDiff))
+	{
+	    *pV1 = u1;
+	    *pV2 = u2;
+	}
+	else // revert
+	{
+	    GraphDisconnect(G[1], u1, u2);
+	    ReBLANT(D[1], G[1], samples, BLANT[1], u1, u2);
+	    GraphConnect(G[1], *pV1, *pV2);
+	    ReBLANT(D[1], G[1], samples, BLANT[1], *pV1, *pV2);
+	}
+    }
+    for(i=0; i < G[1]->numEdges; i++)
+	printf("%d %d\n", G[1]->edgeList[2*i], G[1]->edgeList[2*i+1]);
 }
