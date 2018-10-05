02014016e1c5529c61973bbcda9b35cd66bdc4de
waynebhayes <whayes@uci.edu>
2018-10-05 15:07:32 -0700

    improved synthetic script

--- synthetic.sh
+++ synthetic.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 # Near the start of a run, we need smaller samples (say 1,000) and shorter stagnation (eg, 100).
 # As things improve, we can up the samples to 100,000 and stagnation to eg 10,000.
 # Basic idea seems to be this: we initially want VERY few samples, so we can see which graphlets
@@ -9,27 +9,33 @@
 # slowly start optimizing for those as well.  As we SLOWLY increase the sample size, it's
 # probably good enough to get to 1 million samples.  And you KEEP the same sample size
 # until you stop seeing improvement in the objective at that sample size.
-trap "/bin/rm -f x$$; exit" 0 1 2 3 15
-k=5
+die() { echo "$@" >&2; exit 1
+}
+k=$1
+INPUT="$2"
+baseI=`basename $INPUT`
+OUTPUT="$3"
+baseO=`basename $OUTPUT`
 STAG=1024
-sampleSize=1024
-./blant -k $k -mi -s $sampleSize syeast.random > blant.ryeast.k$k
-./blant -k $k -mi -s $sampleSize networks/syeast.el > blant.syeast.k$k
-score=`paste <(awk '{print $1}' blant.syeast.k$k | sort -n | uniq -c) <(awk '{print $1}' blant.ryeast.k$k | sort -n | uniq -c) | hawk '{sum2+=($3-$1)^2}END{print int(sqrt(sum2))}'`
-while [ $sampleSize -le 1048576 ]; do # 1 Mebisample
+sampleSize=1048576
+[ -f "$OUTPUT" ] || die "output graph $OUTPUT must already exist, and will be modified"
+./blant -k $k -mi -s $sampleSize $INPUT > blant.$baseI.k$k
+./blant -k $k -mi -s $sampleSize $OUTPUT > blant.$baseO.k$k
+score=`paste <(awk '{print $1}' blant.$baseI.k$k | sort -n | uniq -c) <(awk '{print $1}' blant.$baseO.k$k | sort -n | uniq -c) | hawk '{sum2+=($3-$1)^2}END{print int(sqrt(sum2))}'`
+while [ $STAG -le 1048576 ]; do # 1 Mebisample
     make synthetic
-    md5sum syeast.random
-    ./synthetic -s $STAG -k $k networks/syeast.el syeast.random blant.syeast.k$k blant.ryeast.k$k > x$$
-    mv x$$ syeast.random
-    ./blant -k $k -mi -s $sampleSize syeast.random > blant.ryeast.k$k
-    ./blant -k $k -mi -s $sampleSize networks/syeast.el > blant.syeast.k$k
-    newScore=`paste <(awk '{print $1}' blant.syeast.k$k | sort -n | uniq -c) <(awk '{print $1}' blant.ryeast.k$k | sort -n | uniq -c) | hawk '{sum2+=($3-$1)^2}END{print int(sqrt(sum2))}'`
+    md5sum $OUTPUT
+    ./synthetic -s $STAG -k $k $INPUT $OUTPUT blant.$baseI.k$k blant.$baseO.k$k > /tmp/x$$
+    mv /tmp/x$$ $OUTPUT
+    ./blant -k $k -mi -s $sampleSize $OUTPUT > blant.$baseO.k$k
+    ./blant -k $k -mi -s $sampleSize $INPUT > blant.$baseI.k$k
+    newScore=`paste <(awk '{print $1}' blant.$baseI.k$k | sort -n | uniq -c) <(awk '{print $1}' blant.$baseO.k$k | sort -n | uniq -c) | hawk '{sum2+=($3-$1)^2}END{print int(sqrt(sum2))}'`
     if [ $newScore -gt $score ]; then # improvement has stopped at this sample size.
-	sampleSize=`expr $sampleSize \* 4`
-	echo increasing sampleSize to $sampleSize
+	STAG=`expr $STAG \* 4`
+	echo increasing STAG to $STAG
 	score=`parse "$newScore*4"`
-	./blant -k $k -mi -s $sampleSize syeast.random > blant.ryeast.k$k
-	./blant -k $k -mi -s $sampleSize networks/syeast.el > blant.syeast.k$k
+	./blant -k $k -mi -s $sampleSize $OUTPUT > blant.$baseO.k$k
+	./blant -k $k -mi -s $sampleSize $INPUT > blant.$baseI.k$k
     else
 	score="$newScore"
     fi
