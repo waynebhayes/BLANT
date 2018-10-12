bba29c1dff590a57a8455417500f13cd49ee96f9
waynebhayes <whayes@uci.edu>
2018-10-12 14:06:41 -0700

    added looping from k=3 to given k rather than fixed k

--- synthetic.sh
+++ synthetic.sh
@@ -19,23 +19,31 @@ baseO=`basename $OUTPUT`
 STAG=1024
 sampleSize=1048576
 [ -f "$OUTPUT" ] || die "output graph $OUTPUT must already exist, and will be modified"
-./blant -k $k -mi -s $sampleSize $INPUT > blant.$baseI.k$k
-./blant -k $k -mi -s $sampleSize $OUTPUT > blant.$baseO.k$k
+for j in `integers 3 $k`; do
+    ./blant -k $j -mi -s $sampleSize $INPUT > blant.$baseI.k$j
+    ./blant -k $j -mi -s $sampleSize $OUTPUT > blant.$baseO.k$j
+done
 score=`paste <(awk '{print $1}' blant.$baseI.k$k | sort -n | uniq -c) <(awk '{print $1}' blant.$baseO.k$k | sort -n | uniq -c) | awk '{sum2+=($3-$1)^2}END{print int(sqrt(sum2))}'`
 while [ $STAG -le 1048576 ]; do # 1 Mebisample
     make synthetic
     md5sum $OUTPUT
-    ./synthetic -s $STAG -k $k $INPUT $OUTPUT blant.$baseI.k$k blant.$baseO.k$k > /tmp/x$$
-    mv /tmp/x$$ $OUTPUT
-    ./blant -k $k -mi -s $sampleSize $OUTPUT > blant.$baseO.k$k
-    ./blant -k $k -mi -s $sampleSize $INPUT > blant.$baseI.k$k
-    newScore=`paste <(awk '{print $1}' blant.$baseI.k$k | sort -n | uniq -c) <(awk '{print $1}' blant.$baseO.k$k | sort -n | uniq -c) | awk '{sum2+=($3-$1)^2}END{print int(sqrt(sum2))}'`
+    for j in `integers 3 $k`; do
+	./synthetic -s $STAG -k $j $INPUT $OUTPUT blant.$baseI.k$j blant.$baseO.k$j > /tmp/x$$
+	mv /tmp/x$$ $OUTPUT
+    done
+    for j in `integers 3 $k`; do
+	./blant -k $j -mi -s $sampleSize $OUTPUT > blant.$baseO.k$j
+	./blant -k $j -mi -s $sampleSize $INPUT > blant.$baseI.k$j
+    done
+    newScore=`paste <(awk '{print $1}' blant.$baseI.k? | sort -n | uniq -c) <(awk '{print $1}' blant.$baseO.k? | sort -n | uniq -c) | awk '{sum2+=($3-$1)^2}END{print int(sqrt(sum2))}'`
     if [ $newScore -gt $score ]; then # improvement has stopped at this sample size.
 	STAG=`expr $STAG \* 4`
 	echo increasing STAG to $STAG
 	score=`parse "$newScore*4"`
-	./blant -k $k -mi -s $sampleSize $OUTPUT > blant.$baseO.k$k
-	./blant -k $k -mi -s $sampleSize $INPUT > blant.$baseI.k$k
+	for j in `integers 3 $k`; do
+	    ./blant -k $j -mi -s $sampleSize $OUTPUT > blant.$baseO.k$j
+	    ./blant -k $j -mi -s $sampleSize $INPUT > blant.$baseI.k$j
+	done
     else
 	score="$newScore"
     fi
