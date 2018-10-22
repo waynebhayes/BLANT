cb5da825883e649e29c45ac257217838e18fcd45
waynebhayes <whayes@uci.edu>
2018-10-21 22:59:12 -0700

    Pushkar's synthetic.c which takes all values of k

--- synthetic.c
+++ synthetic.c
@@ -12,7 +12,7 @@
 #include "blant.h"
 #include "sets.h"
 
-char USAGE[] = "synthetic -k k -s STAGNATED Gtarget.el Gsynth.el blant.Gt.index blant.Gs.index\n - output is new Gsynth.el\n";
+char USAGE[] = "synthetic -k maxk -s STAGNATED Gtarget.el Gsynth.el [--blant.Gt.index files--] [--blant.Gs.index files--]\n - output is new Gsynth.el\n";
 
 Boolean _supportNodeNames = false;
 
@@ -22,9 +22,12 @@ Boolean _supportNodeNames = false;
 // makes the coding much simpler.
 typedef unsigned char kperm[3]; // The 24 bits are stored in 3 unsigned chars.
 
-static unsigned int _Bk, _k; // _k is the global variable storing k; _Bk=actual number of entries in the canon_map for given k.
-static int _numCanon, _canonList[MAX_CANONICALS];
-static int _numOrbits, _orbitList[MAX_CANONICALS][maxK], _numSamples;
+
+static int _k[maxK]; // stores what values of k have to be considered e.g. [3,4,5,6] or [3,4,5] or [2,7,8]. Will be followed by -1s
+static int _numCanon[maxK];  // canonicals for particular value of k. So for k=5, _numCanon[5-1] stores ~32
+static int _maxNumCanon = -1;  // max number of canonicals
+static int _numSamples = -1;  // same number of samples in each blant index file
+static int _canonList[MAX_CANONICALS];  // <-- *todo* for every 'k'
 static int _stagnated = 1000;
 
 // Here's where we're lazy on saving memory, and we could do better.  We're going to allocate a static array
@@ -35,29 +38,38 @@ static kperm Permutations[maxBk] __attribute__ ((aligned (8192)));
 // Here's the actual mapping from non-canonical to canonical, same argument as above wasting memory, and also mmap'd.
 // So here we are allocating 256MB x sizeof(short int) = 512MB.
 // Grand total statically allocated memory is exactly 1.25GB.
-static short int _K[maxBk] __attribute__ ((aligned (8192)));
+static short int* _K[maxK];
 
 
-// Assuming the global variable _k is set properly, go read in and/or mmap the big global
+// Assuming the global variable _k[] is set properly, go read in and/or mmap the big global
 // arrays related to canonical mappings and permutations.
-void SetGlobalCanonMaps(void)
-{
-    assert(3 <= _k && _k <= 8);
-    _Bk = (1 <<(_k*(_k-1)/2));
-    char BUF[BUFSIZ];
-    int i;
-    _numCanon = canonListPopulate(BUF, _canonList, _k);
-    _numOrbits = orbitListPopulate(BUF, _orbitList, _k);
-    mapCanonMap(BUF, _K, _k);
-    sprintf(BUF, CANON_DIR "/perm_map%d.bin", _k);
-    int pfd = open(BUF, 0*O_RDONLY);
-    kperm *Pf = Mmap(Permutations, _Bk*sizeof(Permutations[0]), pfd);
-    assert(Pf == Permutations);
+void SetGlobalCanonMaps(void){
+	unsigned int _Bk;
+	int i;
+	for(i=0; i<maxK; i++){  // for all values of 'k'
+		if (_k[i] == -1) 
+			break;
+		assert(3 <= _k[i] && _k[i] <= 8);
+		_Bk = (1 <<(_k[i]*(_k[i]-1)/2));
+		if ((_Bk * sizeof(short int)) < 8192)
+			_Bk = 8192 / sizeof(short int);  // aligned_alloc constraint
+		char BUF[BUFSIZ];
+		_numCanon[_k[i]-1] = canonListPopulate(BUF, _canonList, _k[i]);
+		if (_numCanon[_k[i]-1] > _maxNumCanon)  // set max number of canonicals for a k
+			_maxNumCanon = _numCanon[_k[i]-1];
+		_K[_k[i]-1] = (short int*) aligned_alloc(8192, _Bk * sizeof(short int));
+		assert(_K[_k[i]-1] != NULL);
+		mapCanonMap(BUF, _K[_k[i]-1], _k[i]);
+		sprintf(BUF, CANON_DIR "/perm_map%d.bin", _k[i]);
+		int pfd = open(BUF, 0*O_RDONLY);
+		_Bk = (1 <<(_k[i]*(_k[i]-1)/2));  // reset
+		kperm *Pf = Mmap(Permutations, _Bk*sizeof(Permutations[0]), pfd);
+    	assert(Pf == Permutations);
+	}
 }
 
 // Given the big graph G and a set of nodes in V, return the TINY_GRAPH created from the induced subgraph of V on G.
-static TINY_GRAPH *TinyGraphInducedFromGraph(TINY_GRAPH *Gv, GRAPH *G, int *Varray)
-{
+static TINY_GRAPH *TinyGraphInducedFromGraph(TINY_GRAPH *Gv, GRAPH *G, int *Varray){
     unsigned i, j;
     TinyGraphEdgesAllDelete(Gv);
     for(i=0; i < Gv->n; i++) for(j=i+1; j < Gv->n; j++)
@@ -66,38 +78,46 @@ static TINY_GRAPH *TinyGraphInducedFromGraph(TINY_GRAPH *Gv, GRAPH *G, int *Varr
     return Gv;
 }
 
-void ReBLANT(int *D, GRAPH *G, SET **samples, int **Varrays, int **B, int v1, int v2)
-{
-    int j;
-    {
-	int testCount = 0;
-	for(j=0; j<_numCanon; j++) testCount += D[j];
-	assert(testCount == _numSamples);
-    }
+void ReBLANT(int _maxNumCanon, int D[maxK][_maxNumCanon], GRAPH *G, SET ***samples, int ***Varrays, 
+					int _numSamples, int ***BLANT, int v1, int v2){
 
-    int line, s;
-    static TINY_GRAPH *g;
-    static int Varray[maxK];
-    if(!g) g = TinyGraphAlloc(_k);
-    
-    for(s=1; line = Varrays[v1][s], s<=Varrays[v1][0]; s++) if(SetIn(samples[v2], line))
-    {
-	--D[B[line][0]];
-	TinyGraphInducedFromGraph(g, G, B[line]+1);
-	B[line][0] = _K[TinyGraph2Int(g, _k)];
-	++D[B[line][0]];
-    }
+    int i, j, line, s;
+    static TINY_GRAPH *g[maxK];
 
-    {
-	int testCount = 0;
-	for(j=0; j<_numCanon; j++) testCount += D[j];
-	assert(testCount == _numSamples);
+    for (i=0; i<maxK; i++){
+    	if (_k[i] == -1)
+    		break;
+    	
+    	// allocate a tiny graph 
+    	if (!g[_k[i]-1]) 
+    		g[_k[i]-1] = TinyGraphAlloc(_k[i]);
+
+    	{
+    	int testCount = 0;
+    	for(j=0; j<_numCanon[_k[i]-1]; j++)
+    		testCount += D[_k[i]-1][j];
+    	assert(testCount == _numSamples);
+    	}
+
+    	for (s=1; line=Varrays[_k[i]-1][v1][s], s<=Varrays[_k[i]-1][v1][0]; s++)    		
+    		if(SetIn(samples[_k[i]-1][v2], line)){
+    			--D[_k[i]-1][BLANT[_k[i]-1][line][0]];
+    			TinyGraphInducedFromGraph(g[_k[i]-1], G, BLANT[_k[i]-1][line]+1);
+    			BLANT[_k[i]-1][line][0] = _K[_k[i]-1][TinyGraph2Int(g[_k[i]-1], _k[i])];
+    			++D[_k[i]-1][BLANT[_k[i]-1][line][0]];
+    		}
+    	    	
+    	{
+    	int testCount = 0;
+    	for(j=0; j<_numCanon[_k[i]-1]; j++)
+    		testCount += D[_k[i]-1][j];
+    	assert(testCount == _numSamples);
+    	}
     }
 }
 
-
-double PoissonDistribution(double l, int k)
-{
+double PoissonDistribution(double l, int k){
+	//->   (e^(-l) x l^k) / k!
     double r = exp(-l);
     int i;
     for(i=k; i>0; i--) // divide by k!
@@ -105,18 +125,23 @@ double PoissonDistribution(double l, int k)
     return r;
 }
 
-double Objective(int D[2][_numCanon])
-{
-    int i;
+
+double Objective(int _maxNumCanon, int D[2][maxK][_maxNumCanon]){
+    int i,j;
     double logP = 0, sum2 = 0;
-    for(i=0; i<_numCanon; i++)
-    {
-	double pd = PoissonDistribution(D[0][i], D[1][i]);
-	if(pd > 1)
-	    Fatal("umm.... PoissonDistribution returned a number greater than 1");
-	if(pd>0) logP += log(pd); // if we're close, use probability
-	sum2 += SQR(D[0][i] - D[1][i]); // use this one when we're so far away the probability is zero
+
+    for (i=0; i<maxK; i++){
+    	if (_k[i] == -1) break;
+    	for (j=0; j<_numCanon[_k[i]-1]; j++){
+    		double pd = PoissonDistribution(D[0][_k[i]-1][j], D[1][_k[i]-1][j]);
+			if(pd > 1)
+			    Fatal("umm.... PoissonDistribution returned a number greater than 1");
+			if(pd>0) 
+				logP += log(pd); // if we're close, use probability
+			sum2 += SQR(D[0][_k[i]-1][j] - D[1][_k[i]-1][j]); // use this one when we're so far away the probability is zero
+    	}
     }
+
     return sqrt(sum2); //exp(logP);
 }
 
@@ -131,91 +156,139 @@ int main(int argc, char *argv[])
 	exit(1);
     }
 
-    _k = 0;
+    // initialize _k[]
+    for(i=0; i<maxK; i++)
+    	_k[i] = -1; 
 
-    while((opt = getopt(argc, argv, "k:s:")) != -1)
-    {
-	switch(opt)
-	{
-	case 'k': _k = atoi(optarg);
-	    if(!(3 <= _k && _k <= 8)) Fatal("k must be between 3 and 8\n%s", USAGE);
-	    break;
-	case 's': _stagnated = atoi(optarg);
-	    if(!(_stagnated>=10)) Fatal("STAGNATED must be > 10\n%s", USAGE);
-	    break;
-	default: Fatal("unknown option %c\n%s", opt, USAGE);
-	}
+    /*
+	Read max k and stagnation
+    */
+    int kmax;
+    while((opt = getopt(argc, argv, "k:s:")) != -1){
+		switch(opt){
+			case 'k': 
+				kmax = atoi(optarg);
+			    if(!(3 <= kmax && kmax <= 8)) Fatal("k must be between 3 and 8\n%s", USAGE);
+
+			    // _k[] = [3,4,5,6,k]
+			    for(i=3; i<=kmax; i++)
+			    	_k[i-3] = i;
+			    break;
+			case 's': _stagnated = atoi(optarg);
+			    if(!(_stagnated>=10)) Fatal("STAGNATED must be > 10\n%s", USAGE);
+			    break;
+			default: Fatal("unknown option %c\n%s", opt, USAGE);
+		}
     }
-    SetGlobalCanonMaps(); // needs _k to be set
 
-    GRAPH *G[2]; // G[0] is the target, G[1] is the synthetic.
-    for(i=0; i<2; i++)
-    {
-	if(!argv[optind]) Fatal("no input graph file specified\n%s", USAGE);
-	FILE *fpGraph = fopen(argv[optind], "r");
-	if(!fpGraph) Fatal("cannot open graph input file '%s'\n", argv[optind]);
-	// Read it in using native Graph routine.
-	G[i] = GraphReadEdgeList(fpGraph, true, _supportNodeNames); // sparse=true
-	if(_supportNodeNames)
-	    assert(G[i]->name);
-	fclose(fpGraph);
-	optind++;
+    SetGlobalCanonMaps(); // needs _k[] to be set
+
+    GRAPH *G[2]; // G[0] is the target, G[1] is the synthetic
+    for(i=0; i<2; i++){
+		if(!argv[optind]) Fatal("no input graph file specified\n%s", USAGE);
+		FILE *fpGraph = fopen(argv[optind], "r");
+		if(!fpGraph) Fatal("cannot open graph input file '%s'\n", argv[optind]);
+		// Read it in using native Graph routine.
+		G[i] = GraphReadEdgeList(fpGraph, true, _supportNodeNames); // sparse=true
+		if(_supportNodeNames)
+		    assert(G[i]->name);
+		fclose(fpGraph);
+		optind++;
     }
 
     // The distribution of graphlets (squiggly plot vectors)
-    int D[2][_numCanon];
-    for(i=0; i<_numCanon; i++) D[0][i] = D[1][i] = 0;
+    assert(_maxNumCanon != -1);  // this should be set >0 by calling SetGlobalCanonMaps() first
+    int D[2][maxK][_maxNumCanon];
+    for(i=0; i<maxK;i++)
+    	for (j=0; j<_maxNumCanon; j++) 
+    		D[0][i][j] = D[1][i][j] = 0;
 
-    int nSamples[2], **BLANT[2];
-    for(i=0;i<2;i++)
-    {
-	char cmd[BUFSIZ];
-	// Get number of samples
-	sprintf(cmd, "wc -l < %s", argv[optind]);
-	FILE *fp = popen(cmd, "r");
-	assert(fp);
-	fscanf(fp, "%d", &nSamples[i]);
-	assert(nSamples[i] > 0);
-	pclose(fp);
-	fp=fopen(argv[optind], "r");
-	assert(fp);
-	BLANT[i] = (int**)Malloc(nSamples[i] * sizeof(int*));
-	for(line=0; line<nSamples[i]; line++)
-	{
-	    BLANT[i][line] = (int*)Malloc((_k+1)*sizeof(int));
-	    for(j=0; j<_k+1; j++)
-		assert(1 == fscanf(fp, "%d", &(BLANT[i][line][j])));
-	    ++D[i][BLANT[i][line][0]]; // squiggly plot
-	}
-	assert(fscanf(fp, "%d", &line) < 1); // ensure there's nothing left to read.
-	fclose(fp);
-	optind++;
-    }
-    assert(nSamples[0] == nSamples[1]);
-    _numSamples = nSamples[0];
-    for(i=0;i<2;i++)
-    {
-	int testCount = 0;
-	for(j=0; j<_numCanon; j++) testCount += D[i][j];
-	assert(testCount == _numSamples);
+    // expect 2 blant files (target & synthetic for every _k value)
+    // assume all blant files have same number of samples = _numSamples
+    int **BLANT[2][maxK];
+    for(i=0;i<2;i++){
+    	for(j=0; j<maxK; j++){
+    		if(_k[j] == -1)
+    			break;
+
+    		char cmd[BUFSIZ];
+    		sprintf(cmd, "wc -l < %s", argv[optind]);
+    		FILE *fp = popen(cmd, "r");
+    		assert(fp);
+    		if (_numSamples == -1){
+    			fscanf(fp, "%d", &_numSamples);
+    			assert(_numSamples > 0);
+    		}
+    		else{
+    			int tempsamples;
+    			fscanf(fp, "%d", &tempsamples);
+    			assert(tempsamples == _numSamples);
+    		}
+    		pclose(fp);
+			fp=fopen(argv[optind], "r");
+			assert(fp);
+
+			BLANT[i][_k[j]-1] = (int**) Malloc(_numSamples * sizeof(int*));
+			for (line=0; line<_numSamples; line++){
+				BLANT[i][_k[j]-1][line] = (int*) Malloc((maxK+1) * sizeof(int));
+				int l;
+				for (l=0; l<=_k[j]; l++)
+					assert(1 == fscanf(fp, "%d", &(BLANT[i][_k[j]-1][line][l])));
+                assert(BLANT[i][_k[j]-1][line][0] < _maxNumCanon);
+				++D[i][_k[j]-1][BLANT[i][_k[j]-1][line][0]]; // squiggly plot
+			}
+			assert(fscanf(fp, "%d", &line) < 1); // ensure there's nothing left to read.
+			fclose(fp);
+			optind++;
+    	}
+    
     }
 
-    SET **samples = (SET**)Malloc(G[1]->n * sizeof(SET*));
-    for(i=0; i < G[1]->n; i++)
-	samples[i] = SetAlloc(_numSamples);
-    for(line = 0; line < _numSamples; line++)
-	for(j=1; j<=_k; j++)
-	    SetAdd(samples[BLANT[1][line][j]], line);
-
-    int **Varrays = (int**)Malloc(G[1]->n * sizeof(int*));
-    for(i=0; i < G[1]->n; i++)
-    {
-	Varrays[i] = (int*)Malloc((1+SetCardinality(samples[i]))*sizeof(int));
-	Varrays[i][0] = SetToArray(Varrays[i]+1, samples[i]);
+    // check (squiggly vectors count == numsamples)
+    for(i=0; i<2; i++){
+    	for (j=0; j<maxK; j++){
+    		if (_k[j] == -1)
+    			break;
+    		int testCount = 0;
+    		int l;
+    		for (l=0; l<_numCanon[_k[j]-1]; l++)
+    			testCount += D[i][_k[j]-1][l];
+    		assert(testCount == _numSamples);
+    	}
     }
+
+    // store what samples is a particular node-part of
+    SET **samples[maxK];
+    for(i=0; i<maxK; i++){
+    	if (_k[i] == -1) break;
+    	samples[_k[i]-1] = (SET**) Malloc(G[1]->n * sizeof(SET*));
+    }
+
+    for(i=0; i<maxK; i++){
+    	if (_k[i] == -1) break;
+
+    	for (j=0; j<G[1]->n; j++)
+    		samples[_k[i]-1][j] = SetAlloc(_numSamples);
+
+    	for (line=0; line<_numSamples; line++)
+    		for (j=1; j<= _k[i]; j++)
+    			SetAdd(samples[_k[i]-1][BLANT[1][_k[i]-1][line][j]], line);	   // SetAdd(samples[k][nodenum], line);
+    }
+
+    int **Varrays[maxK];
+    for (i=0; i<maxK; i++){
+    	if (_k[i] == -1) break;
+    	Varrays[_k[i]-1] = (int**) Malloc(G[1]->n * sizeof(int*));
+    	
+    	for (j=0; j < G[1]->n; j++){
+    		Varrays[_k[i]-1][j] =  (int*) Malloc((1+SetCardinality(samples[_k[i]-1][j]))* sizeof(int));
+    		Varrays[_k[i]-1][j][0] = SetToArray(Varrays[_k[i]-1][j]+1, samples[_k[i]-1][j]);
+    	}
+    }
+
 
     // while(not done---either some number of iterations, or objective function says we're too far away)
-    double score = Objective(D), startScore = score;
+    double score = Objective(_maxNumCanon, D), startScore = score;
     while(score > 0.6 * startScore) // that's enough progress otherwise we're over-optimizing at this sample size
     {
 	int edge = drand48() * G[1]->numEdges;
@@ -223,14 +296,17 @@ int main(int argc, char *argv[])
 	do {
 	    u1 = drand48()*G[1]->n;
 	    do u2 = drand48()*G[1]->n; while(u1==u2);
-	} while(GraphAreConnected(G[1], u1, u2)); // find a non-edge
-	assert(GraphAreConnected(G[1], v1, v2));
-	GraphDisconnect(G[1], v1, v2); // remove edge e from Gs
-	ReBLANT(D[1], G[1], samples, Varrays, BLANT[1], v1, v2);
-	GraphConnect(G[1], u1, u2);
-	ReBLANT(D[1], G[1], samples, Varrays, BLANT[1], u1, u2);
+	} while(GraphAreConnected(G[1], u1, u2)); // find a non-edge  u1,u2
+	assert(GraphAreConnected(G[1], v1, v2));  // find edge v1,v2
 
-	double newScore = Objective(D);
+
+	GraphDisconnect(G[1], v1, v2); // remove edge e from Gs
+	ReBLANT(_maxNumCanon, D[1], G[1], samples, Varrays, _numSamples, BLANT[1], v1, v2);
+	
+	GraphConnect(G[1], u1, u2);
+	ReBLANT(_maxNumCanon, D[1], G[1], samples, Varrays, _numSamples, BLANT[1], u1, u2);
+
+	double newScore = Objective(_maxNumCanon, D);
 	static int same;
 	if(newScore < score)
 	{
@@ -247,9 +323,9 @@ int main(int argc, char *argv[])
 	{
 	    ++same;
 	    GraphDisconnect(G[1], u1, u2);
-	    ReBLANT(D[1], G[1], samples, Varrays, BLANT[1], u1, u2);
+	    ReBLANT(_maxNumCanon, D[1], G[1], samples, Varrays, _numSamples, BLANT[1], u1, u2);
 	    GraphConnect(G[1], v1, v2);
-	    ReBLANT(D[1], G[1], samples, Varrays, BLANT[1], v1, v2);
+	    ReBLANT(_maxNumCanon, D[1], G[1], samples, Varrays, _numSamples, BLANT[1], v1, v2);
 	}
 	if(same > _stagnated) break;
     }
