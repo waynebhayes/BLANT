a5df96df7291365e8ae4e95d603e6e33f746013e
waynebhayes <whayes@uci.edu>
2018-10-21 23:46:36 -0700

    modified synthetic.sh to be in line with Pushkar's new synthetic.c, and put all temp files in TMPDIR

--- synthetic.sh
+++ synthetic.sh
@@ -11,6 +11,10 @@
 # until you stop seeing improvement in the objective at that sample size.
 die() { echo "$@" >&2; exit 1
 }
+TMPDIR=/tmp/synthetic$$
+trap "/bin/rm -rf $TMPDIR; exit" 0 1 2 3 15
+mkdir $TMPDIR
+
 k=$1
 INPUT="$2"
 baseI=`basename $INPUT`
@@ -19,30 +23,36 @@ baseO=`basename $OUTPUT`
 STAG=1024
 sampleSize=1048576
 [ -f "$OUTPUT" ] || die "output graph $OUTPUT must already exist, and will be modified"
+BLANTFILES_IN=''
+BLANTFILES_OUT=''
 for j in `integers 3 $k`; do
-    ./blant -k $j -mi -s $sampleSize $INPUT > blant.$baseI.k$j
-    ./blant -k $j -mi -s $sampleSize $OUTPUT > blant.$baseO.k$j
+    BLANTFILES_IN="$BLANTFILES_IN $TMPDIR/blant.$baseI.k$j"
+    BLANTFILES_OUT="$BLANTFILES_OUT $TMPDIR/blant.$baseO.k$j"
+    ./blant -k $j -mi -s $sampleSize $INPUT > $TMPDIR/blant.$baseI.k$j
+    ./blant -k $j -mi -s $sampleSize $OUTPUT > $TMPDIR/blant.$baseO.k$j
 done
-score=`paste <(awk '{print $1}' blant.$baseI.k$k | sort -n | uniq -c) <(awk '{print $1}' blant.$baseO.k$k | sort -n | uniq -c) | awk '{sum2+=($3-$1)^2}END{print int(sqrt(sum2))}'`
+score=`for j in $(integers 3 $k); do paste <(awk '{print $1}' $TMPDIR/blant.$baseI.k$j | sort -n | uniq -c) <(awk '{print $1}' $TMPDIR/blant.$baseO.k$j | sort -n | uniq -c); done | awk '{sum2+=($3-$1)^2}END{print int(sqrt(sum2))}'`
 while [ $STAG -le 1048576 ]; do # 1 Mebisample
     make synthetic
     md5sum $OUTPUT
+    ./synthetic -s $STAG -k $j $INPUT $OUTPUT $BLANTFILES_IN $BLANTFILES_OUT > $TMPDIR/x
+    mv $TMPDIR/x $OUTPUT
+    BLANTFILES_IN=''
+    BLANTFILES_OUT=''
     for j in `integers 3 $k`; do
-	./synthetic -s $STAG -k $j $INPUT $OUTPUT blant.$baseI.k$j blant.$baseO.k$j > /tmp/x$$
-	mv /tmp/x$$ $OUTPUT
+	BLANTFILES_IN="$BLANTFILES_IN $TMPDIR/blant.$baseI.k$j"
+	BLANTFILES_OUT="$BLANTFILES_OUT $TMPDIR/blant.$baseO.k$j"
+	./blant -k $j -mi -s $sampleSize $OUTPUT > $TMPDIR/blant.$baseO.k$j
+	./blant -k $j -mi -s $sampleSize $INPUT > $TMPDIR/blant.$baseI.k$j
     done
-    for j in `integers 3 $k`; do
-	./blant -k $j -mi -s $sampleSize $OUTPUT > blant.$baseO.k$j
-	./blant -k $j -mi -s $sampleSize $INPUT > blant.$baseI.k$j
-    done
-    newScore=`paste <(awk '{print $1}' blant.$baseI.k? | sort -n | uniq -c) <(awk '{print $1}' blant.$baseO.k? | sort -n | uniq -c) | awk '{sum2+=($3-$1)^2}END{print int(sqrt(sum2))}'`
+    newScore=`paste <(awk '{print $1}' $TMPDIR/blant.$baseI.k? | sort -n | uniq -c) <(awk '{print $1}' $TMPDIR/blant.$baseO.k? | sort -n | uniq -c) | awk '{sum2+=($3-$1)^2}END{print int(sqrt(sum2))}'`
     if [ $newScore -gt $score ]; then # improvement has stopped at this sample size.
 	STAG=`expr $STAG \* 4`
 	echo increasing STAG to $STAG
 	score=`parse "$newScore*4"`
 	for j in `integers 3 $k`; do
-	    ./blant -k $j -mi -s $sampleSize $OUTPUT > blant.$baseO.k$j
-	    ./blant -k $j -mi -s $sampleSize $INPUT > blant.$baseI.k$j
+	    ./blant -k $j -mi -s $sampleSize $OUTPUT > $TMPDIR/blant.$baseO.k$j
+	    ./blant -k $j -mi -s $sampleSize $INPUT > $TMPDIR/blant.$baseI.k$j
 	done
     else
 	score="$newScore"
