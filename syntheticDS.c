44c04890acf784cb68feb186de1f540643ab0496
Pushkar Gupta <pushkar.moi@gmail.com>
2018-11-25 10:41:34 -0800

    Clust Coff distribution (#57)
    * degree distribution v1
    
    * degree distribution v2
    
    * degree distribution v3
    
    * synth
    
    * SGK v1
    
    * SGK v2
    
    * SGK and FastSGK v3
    
    * sgk bug fix v1
    
    * 1-sgk
    
    * SGK normalization bug fix
    
    * DiffObjective function
    
    * DiffObjective function
    
    * cleanup & new shell script
    
    * cleanup
    
    * clean
    
    * git cleanup, DIff objective function
    
    * cleanup v2
    
    * cleanup v3
    
    * merging v2
    
    * gdv, histograms v1
    
    * hashmap struct, GDV implementation
    
    * GDV implementation v1
    
    * gdv v2
    
    * gdv v3
    
    * gdv v4
    
    * gdv -- ignore cost for disconnected graphlets
    
    * gdv -- param for IGNORE_DISCONNECTED_GRAPHLETS
    
    * gdv final
    
    * bins for the histograms
    
    * bins v2
    
    * bins v2
    
    * multiple graphlet based obj funcs, code cleanup
    
    * clust coff
    
    * clust coff v1
    
    * line 668
    
    * clust coff v3
    
    * clust coff v3
    
    * clust coff v4
    
    * clust coff v4
    
    * clust coff v5
    
    * cc v6
    
    * clust coff distributions objective
    
    * clust coff distribution

--- syntheticDS.c
+++ syntheticDS.c
@@ -101,12 +101,32 @@ int compare_ints(const void* a, const void* b){
     return 0;
 }
 
+int compare_doubles(const void* a, const void* b){
+    // this function is from -> https://en.cppreference.com/w/c/algorithm/qsort
+    double arg1 = *(const double*)a;
+    double arg2 = *(const double*)b;
+ 
+    if (arg1 < arg2) return -1;
+    if (arg1 > arg2) return 1;
+    return 0;
+}
+
 // median
-int getMedian(int* nums, int start, int end){
+int getIntMedian(int* nums, int start, int end){
     // array must have MAX(start+1,end+1) elements
     int n = end-start+1;
     if (n%2 == 0){
         return (nums[start + (n/2)] + nums[start + (n/2) - 1]) / 2.0;
+    }else{
+        return nums[start + (n/2)];
+    }
+}
+
+double getDoubleMedian(double* nums, int start, int end){
+    // array must have MAX(start+1,end+1) elements
+    int n = end-start+1;
+    if (n%2 == 0){
+        return ((double) (nums[start + (n/2)] + nums[start + (n/2) - 1])) / 2.0;
     }else{
         return nums[start + (n/2)];
     }
@@ -122,3 +142,188 @@ double PoissonDistribution(double l, int k){
     return r;
 }
 
+
+double getDoubleBinSize(int n, double localClustCoff[n], double* scratchspace){
+    // sort 
+    // getIQR
+    // return (2IQR) / (n^1/3)
+    //assert(n >= 5);
+
+    double* sorted = scratchspace;
+    memcpy(sorted, localClustCoff, n * sizeof(double));
+    qsort(sorted, n, sizeof(double), compare_doubles);
+
+    double q1, q3;
+
+    if (n%2 == 0){
+        q1 = getDoubleMedian(sorted, 0, (n/2)-1);
+        q3 = getDoubleMedian(sorted, n/2, n-1);
+    }else{
+        q1 = getDoubleMedian(sorted, 0, n/2);
+        q3 = getDoubleMedian(sorted, n/2, n-1);
+    }
+
+    //assert(q3 >= q1);
+    double obs = ((double) (2 * (q3-q1)) / cbrt((double) n));
+    //assert(obs > 0);
+    return obs;
+}
+
+int getIntegerBinSize(int n, int GDVcolumn[n], int* scratchspace){
+    // sort
+    // getIQR
+    // return  (2IQR)/(n^1/3)
+    //assert(n>=5);
+
+    int* sorted = scratchspace;
+    memcpy(sorted, GDVcolumn, n * sizeof(int));
+    qsort(sorted, n, sizeof(int), compare_ints);
+
+    int q1,q3;
+
+    if (n%2 == 0){
+        q1 = getIntMedian(sorted, 0, (n/2)-1);
+        q3 = getIntMedian(sorted, n/2, n-1);
+    }else{
+        q1 = getIntMedian(sorted, 0, n/2);
+        q3 = getIntMedian(sorted, n/2, n-1);
+    }
+
+    //assert(q3 >= q1);
+    double obs = ((double) (2 * (q3-q1)) / cbrt((double) n));
+    int returnVal = (int) ceil(obs);
+
+    if (returnVal < 0)
+        return returnVal;
+
+    if (returnVal < 1)
+        return 1;
+    
+    return returnVal;
+}
+
+
+// old implementation of Clustering Coeffienct Objective
+/*
+double AdjustClustCoff(const int x, const int y, const int connected, GRAPH* G, int localConnections[2][_maxNodes], double avg_cc[2]){
+
+    // Should be called AFTER calling GraphConnect or GraphDisconnect
+    // right now localConnections and avg_cc are in sync.
+
+    assert(abs(connected) == 1);
+    int i, j, node, nodecount, c, nc2;
+
+    double sumchange = 0.0;
+    double oldcc, newcc;
+
+    SET* xn = SetAlloc(G->n);
+    for(i=0; i < G->degree[x]; i++)
+        if (G->neighbor[x][i] != y)
+            SetAdd(xn, G->neighbor[x][i]);
+    
+    nodecount = 0;  // nodes which are connected both to x & y
+    for(i=0; i < G->degree[y]; i++){
+        node = G->neighbor[y][i];
+        if ((node!=x) && (SetIn(xn, node))){
+            nodecount += 1;
+            assert(G->degree[node] >= 2);
+            nc2 = (G->degree[node] * (G->degree[node] - 1))/2;
+            c = localConnections[1][node];  // the original num of connections
+            sumchange += (connected * (1/nc2));
+            localConnections[1][node] = c + connected;  // the new num of connections
+        }
+    }
+
+    SetFree(xn);
+
+    // for x
+    if ((G->degree[x] - connected) < 2){ // original degree
+        c = localConnections[1][x];  // the old connections
+        assert(c == 0);
+        oldcc = 0;
+    }else{
+        nc2 = ((G->degree[x] - connected) * ((G->degree[x] - connected) - 1))/2;
+        c = localConnections[1][x];  // the old connections
+        oldcc = ((double) c) / ((double) nc2);
+    }
+
+    if(G->degree[x] < 2){  // current degree
+        c += (connected * nodecount);
+        assert(c == 0);
+        newcc = 0;
+    }else{
+        nc2 = (G->degree[x] * (G->degree[x] - 1))/2;
+        c += (connected * nodecount);
+        newcc = ((double) c) / ((double) nc2);
+    }
+    localConnections[1][x] = c;
+    sumchange += (newcc-oldcc);
+
+
+    // for y
+    if ((G->degree[y] - connected) < 2){ // original degree
+        c = localConnections[1][y];  // the old connections
+        assert(c == 0);
+        oldcc = 0;
+    }else{
+        nc2 = ((G->degree[y] - connected) * ((G->degree[y] - connected) - 1))/2;
+        c = localConnections[1][y];  // the old connections
+        oldcc = ((double) c) / ((double) nc2);
+    }
+
+    if(G->degree[y] < 2){  // current degree
+        c += (connected * nodecount);
+        assert(c == 0);
+        newcc = 0;
+    }else{
+        nc2 = (G->degree[y] * (G->degree[y] - 1))/2;
+        c += (connected * nodecount);
+        newcc = ((double) c) / ((double) nc2);
+    }
+    localConnections[1][y] = c;
+    sumchange += (newcc-oldcc);
+
+    
+    // sanity check
+    int ideal_con[G->n];
+    getConnections(G, ideal_con); // slow!
+    for(i=0; i<G->n; i++)
+        assert(ideal_con[i] == localConnections[1][i]);
+
+    avg_cc[1] += (sumchange / G->n);
+    assert((avg_cc[1] >= 0) && (avg_cc[1] <= 1));
+    return fabs(avg_cc[1] - avg_cc[0]);
+}
+*/
+
+/*
+double ClustCoffObjective(GRAPH* G[2], int localConnections[2][_maxNodes], double avg_cc[2]){
+
+    // old implementation : compute fabs() b/w Average clust coffs of target & synthetic
+
+    double t;
+    double cc_sum[2] = {0.0, 0.0};
+    int i, j, degree, nc2;
+
+    for(i=0; i<2; i++){
+        for(j=0; j < G[i]->n; j++){
+            degree = G[i]->degree[j];
+            nc2 = (degree * (degree-1)) / 2;
+            if (degree > 1)
+                t = ((double) localConnections[i][j]) / ((double) nc2);  // local clustering coefficient
+            else
+                t = 0.0;
+            
+            assert((t >= 0) && (t <= 1));
+            cc_sum[i] += t;
+        }
+    }
+
+    avg_cc[0] = cc_sum[0] / (G[0]->n);
+    avg_cc[1] = cc_sum[1] / (G[1]->n);
+
+    fprintf(stderr, "clustering coefficients: target=%g synthetic=%g\n", avg_cc[0], avg_cc[1]);
+
+    double returnVal = (double) fabs(avg_cc[0] - avg_cc[1]);
+    return returnVal;
+}*/
