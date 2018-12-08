8d93e669da3c5482ae16bb71a9c85e9f4d79740a
waynebhayes <whayes@uci.edu>
2018-12-08 12:32:59 -0800

    added make-ER-graph.sh

---
+++ make-ER-graph.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+N=`count.el "$1" | awk '{print $1}'`
+cat "$1" | hawk 'END{while(n<NR){v=u=int('$N'*rand());while(v==u)v=int('$N'*rand()); x=MIN(u,v);y=MAX(u,v);if(!seen[x" "y]){n++;print x,y;seen[x" "y]=1}}}'
