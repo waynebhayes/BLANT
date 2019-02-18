9246c6222648b89a9cc69d81e116dfd029121e0b
hanweny <hanweny@uci.edu>
2019-02-18 11:17:51 -0800

    Add Dynamic Array(foint) and test file (#79)

---
+++ libwayne/src/dynarray.c
@@ -0,0 +1,100 @@
+#include "misc.h"
+#include "dynarray.h"
+#include <assert.h>
+#include <malloc.h>
+#include <stdio.h>
+
+ARRAY *ArrayAlloc(int initNumobjs)
+{
+	ARRAY *A = Malloc(sizeof(ARRAY));
+	A->size = 0;
+	A->maxSize = initNumobjs;
+	A->array = Calloc(initNumobjs, sizeof(foint));
+	return A;
+}
+
+void ArrayFree(ARRAY *A) 
+{
+	free(A->array);
+	free(A);
+}
+
+int ArraySize(ARRAY *A) {return A->size;}
+
+foint ArrayAdd(ARRAY *A, foint new)
+{
+	if(A->size==A->maxSize)
+	{
+		A->maxSize *= 2;
+		A->array = Realloc(A->array, A->maxSize * sizeof(foint));
+	}
+	A->array[A->size++] = new;
+	return new;
+}
+
+foint ArraySet(ARRAY *A, int pos, foint new)
+{
+	assert(pos < A->size && pos >= 0);
+	A->array[pos] = new;
+	return new;
+}
+
+foint ArrayAt(ARRAY *A, int pos) 
+{
+	assert(pos < A->size && pos >= 0);
+	return A->array[pos];
+}
+
+ARRAY *ArrayAppend(ARRAY *C, ARRAY *A, ARRAY *B)
+{
+	if(!C)
+	C = ArrayAlloc(A->size + B->size);
+	assert(C->maxSize >= A->size + B->size);
+	int i;
+	for(i=0; i<A->size; i++) 
+		C->array[i] = A->array[i];
+	for(i=0; i<B->size; i++)
+		C->array[A->size + i] = B->array[i];
+	C->size = C->maxSize;
+	return C;
+}
+
+foint ArrayRemoveAt(ARRAY *A, int pos)
+{
+	assert(pos < A->size && pos >= 0);
+	foint elem = A->array[pos];
+	int i;
+	for(i=pos; i<A->size-1; i++)
+		A->array[i] = A->array[i+1];
+	--A->size;
+	return elem;
+}
+
+foint ArrayRemove(ARRAY *A, foint elem, pCmpFcn cmp)
+{
+	int i;
+	for(i=0; i<A->size; i++)
+		if(cmp(A->array[i], elem) == 0) 
+		{
+			ArrayRemoveAt(A, i);
+			break;
+		}
+	return elem;
+}
+
+foint ArrayRemoveAll(ARRAY *A, foint elem, pCmpFcn cmp)
+{
+	int i, j, num=0;
+	for(i=0; i<A->size; i++)
+	{
+		j = i+num;
+		while(j<A->size && cmp(A->array[j++], elem) == 0) num++;
+		if(i+num >= A->size) break;
+		A->array[i] = A->array[i+num];
+	}
+	A->size -= num;
+	return elem;
+}
+
+
+
