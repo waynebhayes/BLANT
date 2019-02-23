29b4ad786d7fce26a2506733b38c79de5fb5a28f
Tuong Do <tuongdo203@gmail.com>
2019-02-22 18:31:33 -0800

    Starting version of faye.c

--- blant.c
+++ blant.c
@@ -54,6 +54,7 @@ char _sampleFileEOF;
 // this*k is the number of steps in the Reservoir walk. 8 seems to work best, empirically.
 #define RESERVOIR_MULTIPLIER 8
 #endif
+#define SAMPLE_FAYE 6 // MCMC Algorithm estimates graphlet frequency with a random walk
 
 #define MAX_TRIES 100		// max # of tries in cumulative sampling before giving up
 
@@ -338,6 +339,117 @@ static SET *SampleGraphletNodeBasedExpansion(SET *V, int *Varray, GRAPH *G, int 
 	    if(!SetIn(outSet, v2) && !SetIn(V, v2))
 		SetAdd(outSet, (outbound[nOut++] = v2));
 	}
+    }
+    assert(i==k);
+#if PARANOID_ASSERTS
+    assert(SetCardinality(V) == k);
+    assert(nOut == SetCardinality(outSet));
+#endif
+    return V;
+}
+
+// modelled after faye by Tuong Do
+static SET *SampleGraphletFaye(SET *V, int *Varray, GRAPH *G, int k, int whichCC)
+{
+    /* Faye: Add a visited array to keep track of nodes. Initialize to 0 */
+    int visited[G->n];
+    static SET *outSet;
+    static int numIsolatedNodes;
+    if(!outSet)
+       outSet = SetAlloc(G->n);  // we won't bother to free this since it's static.
+    else if(G->n > outSet->n)
+	SetResize(outSet, G->n);
+    else
+	SetEmpty(outSet);
+    int v1, v2, i;
+    int nOut = 0, outbound[G->n]; // vertices one step outside the boundary of V
+    assert(V && V->n >= G->n);
+    SetEmpty(V);
+    int edge;
+    do {
+	edge = G->numEdges * RandomUniform();
+	v1 = G->edgeList[2*edge];
+    } while(!SetIn(_componentSet[whichCC], v1));
+    v2 = G->edgeList[2*edge+1];
+    SetAdd(V, v1); Varray[0] = v1;
+    SetAdd(V, v2); Varray[1] = v2;
+
+    /* Faye: Mark v1 and v2 as visited */
+    visited[v1] = 1;
+    visited[v2] = 1;    
+
+    // The below loops over neighbors can take a long time for large graphs with high mean degree. May be faster
+    // with bit operations if we stored the adjacency matrix... which may be too big to store for big graphs. :-(
+    for(i=0; i < G->degree[v1]; i++)
+    {
+	int nv1 =  G->neighbor[v1][i];
+	if(nv1 != v2)
+	{
+	    assert(!SetIn(V, nv1)); // assertion to ensure we're in line with faye
+	    if (!visited[nv1]) { /* Faye: Check if it's visited */
+            SetAdd(outSet, (outbound[nOut++] = nv1));
+            visited[nv1] = 1;
+        }
+	}
+    }
+    for(i=0; i < G->degree[v2]; i++)
+    {
+	int nv2 =  G->neighbor[v2][i];
+	if(nv2 != v1 && !SetIn(outSet, nv2))
+	{
+	    assert(!SetIn(V, nv2)); // assertion to ensure we're in line with faye
+	    if (!visited[nv2]) { /* Faye: Check if it's visited */
+            SetAdd(outSet, (outbound[nOut++] = nv2));
+            visited[nv2] = 1;
+        }
+	}
+    }
+    for(i=2; i<k; i++)
+    {
+	int j;
+	if(nOut == 0) // the graphlet has saturated it's connected component
+	{
+	    assert(SetCardinality(outSet) == 0);
+	    assert(SetCardinality(V) < k);
+#if ALLOW_DISCONNECTED_GRAPHLETS
+	    /* Faye: check if the random node is visited instead 
+        *while(SetIn(V, (j = G->n*RandomUniform()))    
+        */
+        while(visited[(j = G->n*RandomUniform())])
+		; // must terminate since k <= G->n
+	    outbound[nOut++] = j;
+	    j = 0;
+#else
+	    static int depth;
+	    depth++;
+	    // must terminate eventually as long as there's at least one connected component with >=k nodes.
+	    assert(depth < MAX_TRIES); // graph is too disconnected
+	    V = SampleGraphletNodeBasedExpansion(V, Varray, G, k, whichCC);
+	    depth--;
+	    // Ensure the damn thing really *is* connected.
+	    TINY_GRAPH *T = TinyGraphAlloc(k);
+	    TinyGraphInducedFromGraph(T, G, Varray);
+	    assert(NumReachableNodes(T,0) == k);
+	    TinyGraphFree(T);
+	    return V;
+#endif
+	}
+	else
+	    j = nOut * RandomUniform();
+	v1 = outbound[j];
+	SetDelete(outSet, v1);
+	SetAdd(V, v1); Varray[i] = v1;
+	outbound[j] = outbound[--nOut];	// nuke v1 from the list of outbound by moving the last one to its place
+	for(j=0; j<G->degree[v1];j++) // another loop over neighbors that may take a long time...
+	{
+	    v2 = G->neighbor[v1][j];
+        /* Faye: check if it's invisted instead
+        * if(!SetIn(outSet, v2) && !SetIn(V, v2)) */
+        if (!visited[v2]) {
+		    SetAdd(outSet, (outbound[nOut++] = v2));
+	        visited[v2] = 1;
+        }
+    }
     }
     assert(i==k);
 #if PARANOID_ASSERTS
@@ -1120,6 +1232,9 @@ void SampleGraphlet(GRAPH *G, SET *V, unsigned Varray[], int k) {
 		case SAMPLE_NODE_EXPANSION:
 			SampleGraphletNodeBasedExpansion(V, Varray, G, k, cc);
 			break;
+		case SAMPLE_FAYE:
+			SampleGraphletFaye(V, Varray, G, k, cc);
+			break;
 		case SAMPLE_RESERVOIR:
 			SampleGraphletLuBressanReservoir(V, Varray, G, k, cc); // pretty slow but not as bad as unbiased
 			break;
@@ -1681,6 +1796,8 @@ int main(int argc, char *argv[])
 		if (_sampleMethod != -1) Fatal("Tried to define sampling method twice");
 		else if (strncmp(optarg, "NBE", 3) == 0)
 			_sampleMethod = SAMPLE_NODE_EXPANSION;
+		else if (strncmp(optarg, "FAYE", 4) == 0)
+			_sampleMethod = SAMPLE_FAYE;
 		else if (strncmp(optarg, "EBE", 3) == 0)
 			_sampleMethod = SAMPLE_EDGE_EXPANSION;
 		else if (strncmp(optarg, "MCMC", 3) == 0)
---
+++ sampleGraphletNodeBasedExpansion.c
@@ -0,0 +1,17 @@
+#include <sys/file.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "misc.h"
+#include "tinygraph.h"
+#include "graph.h"
+#include "heap.h"
+#include "blant.h"
+#include "queue.h"
+#include "multisets.h"
+#include "sorts.h"
+
+
