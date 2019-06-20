0493187f7984d5a83da803b32f84d5e0d2b26f2c
waynebhayes <whayes@uci.edu>
2019-06-20 16:14:05 -0700

    add BLANT_DIR to orcaTest regresison

--- regression-tests/orcaNumbering/test.sh
+++ regression-tests/orcaNumbering/test.sh
@@ -2,7 +2,7 @@
 
 MYTMPDIR=$(mktemp -d)
 trap "rm -rf $MYTMPDIR" EXIT
-
+export BLANT_DIR=../..
 for k in 3 4 5; do
     echo "Testing ODV output matches ORCA for k=${k}"
     $BLANT_DIR/blant -mo -k $k -s NBE -n 100000 $BLANT_DIR/regression-tests/orcaNumbering/$k.el | sort -n > $MYTMPDIR/$k.odv
