486348f93aba0cb24fd5be6e79672f2428ac1542
Professor Wayne B. Hayes, UC Irvine <whayes@uci.edu>
2019-06-21 14:03:56 -0700

    Update test.sh

--- regression-tests/orcaNumbering/test.sh
+++ regression-tests/orcaNumbering/test.sh
@@ -2,7 +2,7 @@
 
 MYTMPDIR=$(mktemp -d)
 trap "rm -rf $MYTMPDIR" EXIT
-export BLANT_DIR=../..
+export BLANT_DIR=. # Remember, this script is run from the *main* repo, not the directory it resides in, so blant is ./blant
 for k in 3 4 5; do
     echo "Testing ODV output matches ORCA for k=${k}"
     $BLANT_DIR/blant -mo -k $k -s NBE -n 100000 $BLANT_DIR/regression-tests/orcaNumbering/$k.el | sort -n > $MYTMPDIR/$k.odv
