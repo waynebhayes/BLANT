2a923ba03ed1a47f85edc226623cc3ff0eab41f6
Brennan Tracy <HBBrennan@users.noreply.github.com>
2019-06-21 21:38:54 -0700

    Use python3 string formatting (#117)

--- regression-tests/orcaNumbering/test.py
+++ regression-tests/orcaNumbering/test.py
@@ -5,9 +5,9 @@ num_connected_orbits = {3: 3, 4: 11, 5: 58}
 start_index = {3: 1, 4: 4, 5: 15}
 exit_code = 0
 for k in [3, 4, 5]:
-    odv = np.loadtxt(f"{sys.argv[1]}/{k}.odv", delimiter = ' ', usecols=range(1, num_connected_orbits[k]+1))
-    orca = np.loadtxt(f"regression-tests/orcaNumbering/{k}.orca", delimiter = ' ', usecols=range(start_index[k], start_index[k] + num_connected_orbits[k]))
+    odv = np.loadtxt("{0}/{1}.odv".format(sys.argv[1], k), delimiter = ' ', usecols=range(1, num_connected_orbits[k]+1))
+    orca = np.loadtxt("regression-tests/orcaNumbering/{0}.orca".format(k), delimiter = ' ', usecols=range(start_index[k], start_index[k] + num_connected_orbits[k]))
     if not (np.array_equal(np.array(odv, dtype=bool), np.array(orca, dtype=bool))):
-        print(f"Error: ODV for k={k} doesn't match ORCA")
+        print("Error: ODV for k={0} doesn't match ORCA".format(k))
         exit_code = 1
 sys.exit(exit_code)
