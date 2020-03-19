3d08559179619c8e8aa7d096e70f1bf56c1f5202
waynebhayes <whayes@uci.edu>
2020-03-19 09:56:00 -0700

    remove nice and update NUM_FAIL rather than RETURN

--- regression-test-all.sh
+++ regression-test-all.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-RETURN=0
+NUM_FAIL=0
 for dir in regression-tests/*; do
     [ -d "$dir" ] || continue;
     echo --- in directory $dir ---
     for r in $dir/*.sh; do
 	echo --- running test $r ---
-	if nice -19 "$r"; then
+	if "$r"; then
 	    :
 	else
-	    RETURN=1
+	    NUM_FAIL=`expr $NUM_FAIL + 1`
 	fi
     done
 done
-exit $RETURN
+exit $NUM_FAIL
