06ea7276dd8930cd112ea0304906c0a39893f5a3
waynebhayes <whayes@uci.edu>
2020-11-02 18:14:59 -0800

    Making regression-tests more standard.. a bit

--- regression-test-all.sh
+++ regression-test-all.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-if [ -f git-at -a `git log -1 --format=%at` -eq `tail -1 git-at` ]; then
+if [ -f git-at ] && [ `wc -l < git-at` -eq 2 -a `git log -1 --format=%at` -eq `tail -1 git-at` ]; then
     echo -n "Repo unchanged; returning same status code as "
     tail -1 git-at | xargs -I{} date -d @{} +%Y-%m-%d-%H:%M:%S
     exit `head -1 git-at`
 fi
-
-die() { echo "FATAL ERROR: $@" >&2; exit 1
-}
-warn() { echo "WARNING: $@" >&2;
-}
+USAGE="USAGE: $0 [ -make ] [ -x BLANT_EXE ][ list of tests to run, defaults to regression-tests/*/*.sh ]"
+source ~/bin/misc.sh
 PATH=`pwd`:`pwd`/scripts:$PATH
 export PATH
 
@@ -17,37 +14,49 @@ MAKE=false
 while [ $# -gt -0 ]; do
     case "$1" in
     -make) MAKE=true; shift;;
-    *) [ -x "$1" -o "$MAKE" = true ] || die "unknown argument '$1'; valid arguments are '-make', and an optional executable filename"
-	EXE="$1"; shift;;
+    -x) EXE="$2"; shift 2;;
+    -*) die "unknown option '$1";;
+    *) break;;
     esac
 done
+[ -x "$EXE" -o "$MAKE" = true ] || die "Executable '$EXE' must exist or you must specify -make"
 
-export EXE
-export CORES=${CORES:=`cpus 2>/dev/null | awk '{c2=int($1/2); print c2?c2:1}'`}
+CORES=${CORES:=`cpus 2>/dev/null | awk '{c2=int($1/2); if(c2>0)print c2; else print 1}'`}
 [ "$CORES" -gt 0 ] || die "can't figure out how many cores this machine has"
-echo "Using $CORES as number of cores"
+MAKE_CORES=$CORES
+
+echo "Using $MAKE_CORES cores to make and $CORES cores for regression tests"
+export EXE CORES MAKE_CORES
+
 if $MAKE ; then
-	make realclean
-	make all || die "failed to make"
+    make realclean
+    make all || die "failed to make"
 fi
 
 [ -x "$EXE" ] || die "no executable '$EXE' exists to test!"
 
 NUM_FAILS=0
-for REG_DIR in regression-tests/*; do
-    if [ -d "$REG_DIR" ]; then
-	export REG_DIR
-	echo --- in directory $REG_DIR ---
-	for r in $REG_DIR/*.sh; do
-	    echo --- running test $r ---
-	    if "$r"; then
-		:
-	    else
-		(( NUM_FAILS+=$? ))
-	    fi
-	done
+STDBUF=''
+if which stdbuf >/dev/null; then
+    STDBUF='stdbuf -oL -eL'
+fi
+if [ $# -eq 0 ]; then
+    set regression-tests/*/*.sh
+fi
+for r
+do
+    REG_DIR=`dirname "$r"`
+    NEW_FAILS=0
+    export REG_DIR
+    echo --- running test $r ---
+    if eval $STDBUF "$r"; then # force output and error to be line buffered
+	:
+    else
+	NEW_FAILS=$?
+	(( NUM_FAILS+=$NEW_FAILS ))
     fi
+    echo --- test $r incurred $NEW_FAILS failures, cumulative failures is $NUM_FAILS ---
 done
-echo Number of failures: $NUM_FAILS
+echo Total number of failures: $NUM_FAILS
 (echo $NUM_FAILS; git log -1 --format=%at) > git-at
 exit $NUM_FAILS
