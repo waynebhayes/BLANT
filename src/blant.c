072fbf34e7b3c8b9958441d1f913131697d0aa6c
Ruicheng <lrc1999@icloud.com>
2020-11-11 10:30:33 +0800

    added output modes contraint for -w1 mode which only supports -mi and -mj output modes so far

--- src/blant.c
+++ src/blant.c
@@ -952,7 +952,7 @@ int main(int argc, char *argv[])
 	}
     }
 
-    if (_windowSize == 1 && _k <= 5) Fatal("k is %d but must be between larger than 5 for window size of 1 since there are no unambiguous graphlets for k<=5",_k); 
+    if (_windowSize == 1 && _k <= 5) Fatal("k is %d but must be between larger than 5 for window size of 1 since there are no unambiguous graphlets for k<=5",_k);
 
     if(_seed == -1) _seed = GetFancySeed(false);
     // This only seeds the main thread; sub-threads, if they exist, are seeded later by "stealing"
@@ -1086,6 +1086,10 @@ int main(int argc, char *argv[])
     }
 #endif
     if(_windowSize == 1) {
+        if (_outputMode != indexGraphlets && _outputMode != indexOrbits) {
+            Fatal("currently only -mi and -mj output modes are supported for -w1 option");
+//            _outputMode = indexGraphlets;
+        }
         _numWindowRepArrSize = 50;
         _windowReps = Calloc(_numWindowRepArrSize, sizeof(int*));
         for(i=0; i<_numWindowRepArrSize; i++) _windowReps[i] = Calloc(_k+1, sizeof(int));
