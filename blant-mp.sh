2d789261f7c11f9c52cff6fda428c2a464cdca95
waynebhayes <whayes@uci.edu>
2020-12-28 08:17:28 -0800

    adding blant-mp.sh

---
+++ blant-mp.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+USAGE="USAGE: $0 blant-exec [blant args without specifying mode, which must be -mp]"
+
+#output looks like:
+#P  u:v    e  o:p   q:r   x:y
+#P 680:265 0 37:37 38:39 517:476
+#P 680:265 0 37:37 38:39 517:25
+#P 265:517 1 37:38 37:39 680:476
+BLANT=$1; shift
+
+$BLANT -mp "$@" | hawk '{
+	G[$2]=$3;
+	#PG4[$2][$4][$5][$6]=1
+	#PG[$2][$4][$5" "$6]=1 # PG=PredictGraph
+	PG[$2][$4][$6]=1 # PG=PredictGraph, maybe q:r is irrelevant?
+    }
+    END {
+	for(uv in PG) {
+	    printf "%s %d",uv,G[uv]
+	    for(op in PG[uv]) {
+		#TP4 = 0 # TP=TotalPredict of edge-orbit-pair (o,p) on node pair (u,v) in G
+		#for(qr in PG4[uv][op]) TP4 += length(PG4[uv][op][qr]) # all the 1s of the xys
+		printf "\t%s %d",op,length(PG[uv][op])
+		#if(length(PG[uv][op])!=TP4)printf("ERROR");
+	    }
+	    print ""
+	}
+    }'
