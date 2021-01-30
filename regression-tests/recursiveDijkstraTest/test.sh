290c4bd88180d1d9b03151584d780b525049be17
waynebhayes@github.com <whayes@uci.edu>
2021-01-29 17:20:20 -0800

    now actually REALLY soft fail on Dijkstra with no SIM

--- regression-tests/recursiveDijkstraTest/test.sh
+++ regression-tests/recursiveDijkstraTest/test.sh
@@ -12,10 +12,13 @@ if module avail 2>/dev/null; then
     module unload python
     module load python/3.6.8
 fi
-./Dijkstracmd
-rm -f *.pickle
+if ./Dijkstracmd; then
+    rm -f *.pickle
 
-echo "=====Comparing .log with past log file====="
-for i in oldlog.log seed7/*.log; do echo `sed 's/time:[0-9.:]*//' $i | sort | md5sum` $i; done | 
-    awk 'BEGIN{FAIL=1}{print NR, $0; md5[NR]=$1}END{exit( md5[1]!=md5[2] ? FAIL : !FAIL)}'
-exit $?
+    echo "=====Comparing .log with past log file====="
+    for i in oldlog.log seed7/*.log; do echo `sed 's/time:[0-9.:]*//' $i | sort | md5sum` $i; done | 
+	awk 'BEGIN{FAIL=1}{print NR, $0; md5[NR]=$1}END{exit( md5[1]!=md5[2] ? FAIL : !FAIL)}'
+    exit $?
+else
+    [ $? = 99 ] && exit 0 # ignoring failed Dijkstra
+fi
