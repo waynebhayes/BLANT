7e5eab10788002a124682a19457b93388ca6b79d
waynebhayes@github.com <whayes@uci.edu>
2021-04-13 15:37:48 -0700

    added predict-stub

---
+++ src/blant-predict-stub.c
@@ -0,0 +1,12 @@
+#include "misc.h"
+#include "blant.h"
+#include "tinygraph.h"
+#include "graph.h"
+
+void Predict_Init(GRAPH *G){Fatal("prediction not supported by your version of gcc");}
+void Predict_FlushMotifs(GRAPH *G){Fatal("prediction not supported by your version of gcc");};
+void Predict_ProcessLine(GRAPH *G, char line[]){Fatal("prediction not supported by your version of gcc");};
+
+void Predict_AccumulateMotifs(GRAPH *G, unsigned Varray[], TINY_GRAPH *g, Gint_type Gint, int GintOrdinal){Fatal("prediction not supported by your version of gcc");};
+int  Predict_Merge(GRAPH *G){Fatal("prediction not supported by your version of gcc");};
+
