a01a018390c2d1aff51262b97cd0dcabf0df07fc
waynebhayes <whayes@uci.edu>
2021-11-11 17:19:51 -0800

    prettying up windowDistribution test

--- regression-tests/windowDistribution/windowDistributionTest.sh
+++ regression-tests/windowDistribution/windowDistributionTest.sh
@@ -21,13 +21,15 @@ declare -a fnames=("SCerevisiae.el" "AThaliana.el" "CElegans.el")
 for k in {3..6}; do
     for nnw in "${fnames[@]}"
     do
-        if ! [ `./blant -k$k -md -sMCMC -t $CORES -n$n networks/$nnw | tr ' ' '\n' | awk '{sum+=$1} END {print 1*sum}'` -eq $n ]; then
+        outLines=`./blant -k$k -md -sMCMC -t $CORES -n$n networks/$nnw | tr ' ' '\n' | awk '{sum+=$1} END {print 1*sum}'`
+        if [ $outLines -ne $n ]
+	then
             echo "Distribution table entries do not sum up to $n" >&2
             echo "cmd: ./blant -k$k -md -sMCMC -t $CORES -n$n networks/$nnw" >&2
             exit 1
         fi
-        `./blant -k$k -md -sMCMC -t $CORES -r$seed -n$n networks/$nnw > $TEST_DIR/blant_distr1.txt`
-        `./blant -k$k -md -sMCMC -t $CORES -r$seed -n$n networks/$nnw > $TEST_DIR/blant_distr2.txt`
+        ./blant -k$k -md -sMCMC -t $CORES -r$seed -n$n networks/$nnw > $TEST_DIR/blant_distr1.txt
+        ./blant -k$k -md -sMCMC -t $CORES -r$seed -n$n networks/$nnw > $TEST_DIR/blant_distr2.txt
         if ! [ `diff $TEST_DIR/blant_distr1.txt $TEST_DIR/blant_distr2.txt | wc -l` -eq 0 ]; then
             echo "Distribution table differs for the same random seed $seed" >&2
             echo "cmd: ./blant -k$k -md -sMCMC -t $CORES -r$seed -n$n networks/$nnw" >&2
