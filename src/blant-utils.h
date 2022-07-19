3fef12773092e8822e50d4cac39b55e42660e2f0
Professor Wayne B. Hayes, UC Irvine <whayes@uci.edu>
2022-07-19 05:14:48 -0700

    Revert "Pretty much everything in between the VLDB paper (March 2022) and the ICDE paper (July 2022) (#163)" (#164)
    This reverts commit dedd0822c1c6381c07d3f63c14ce1329fc543ae6.

--- src/blant-utils.h
+++ src/blant-utils.h
@@ -3,25 +3,24 @@
 
 #include "blant.h"
 
-typedef struct nwhn { // this struct is needed for sorting
+typedef struct nwd { // this struct is needed for enumerateNodeOrder
     int node;
     int heur;
-    char *name;
-} node_whn;
+} node_wheur;
 
 Boolean arrayIn(int* arr, int size, int item);
-void printIntArray(int* arr, int n, char* name);
 void ExtractPerm(char perm[_k], int i);
 void InvertPerm(char inverse[_k], const char perm[_k]);
 TINY_GRAPH *TinyGraphInducedFromGraph(TINY_GRAPH *Gv, GRAPH *G, int *Varray);
 int getMaximumIntNumber(int K);
 int asccompFunc(const foint i, const foint j);
 int descompFunc(const void *a, const void *b);
-int nwhn_des_alph_comp_func(const void *a, const void *b);
-int nwhn_des_rev_comp_func(const void *a, const void *b);
-int nwhn_asc_alph_comp_func(const void *a, const void *b);
-int nwhn_asc_rev_comp_func(const void *a, const void *b);
+int nwh_descompFunc(const void *a, const void *b);
+int nwh_asccompFunc(const void *a, const void *b);
 void SetGlobalCanonMaps(void);
 void LoadMagicTable();
+int* enumerateNodeOrder(GRAPH *G);
+void enumerateNodeOrderHelper(GRAPH *G, node_wheur* orderArray, int start, int end, int layer);
+void antidupFillNextStepSet(SET **next_step, SET **deg_set, GRAPH *G, int *prev_nodes_array, int prev_nodes_count, int *node_order);
 
 #endif
