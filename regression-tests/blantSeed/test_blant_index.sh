32187db66e1528ee5a1407f2b8ece3931810914d
Patrick Wang <wang.patrick57@gmail.com>
2022-07-21 00:35:09 -0700

    Added regression tests (#166)
    * generalized degree to heuristic
    
    * implemented simple ODV parsing w/ some sparse bugs
    
    * removed antidup algorithm from main code (but kept function around); added a lot of comments
    
    * removed libwayne
    
    * added libwayne submodule
    
    * implemented odv heuristic retrieval and fixed parse bug
    
    * removed libwayne cleanly this time
    
    * finished clean remove
    
    * readded libwayne after clean remove
    
    * implemented heuristics but also may not work
    
    * finished adding comments
    
    * reverted Makefile
    
    * removed libwayne
    
    * added libwayne back
    
    * added importance.c back to Makefile
    
    * fixes while testing odv parsing
    
    * removed libwayne
    
    * readded libwayne
    
    * added thousandth opt
    
    * test
    
    * added yeast orca4 files
    
    * removed patching stuff
    
    * added seeding paper algorithms and data to BLANT
    
    * added seeding paper to readme
    
    * reverted Makefile
    
    * quick fix to readme
    
    * Delete .firsttime
    
    .firsttime shoudl not be in the repo
    
    * cleaned up some files
    
    * removed two final files
    
    * made changes to README
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * removed more files
    
    * removed seed7
    
    * removed .so file
    
    * wrote alphabetic tie breaking code and base node ordering code
    
    * wrote stair variations and some printers
    
    * wrote bno code
    
    * base node orbit code (no settings yet)
    
    * full seed mining code migrated
    
    * instructions for icde
    
    * modified gitignore, added odv files to seed mining, modified insturctions, cleaned up blant code
    
    * new regression tests and added more instructions
    
    * fixed regression test
    
    * modified gitignore to not ignore regression tests and added blantSeed + odvSeeds regression tests
    
    Co-authored-by: Arthur Lafrance <lafrancearthur@gmail.com>
    Co-authored-by: Arthur Lafrance <38890624+arthurlafrance@users.noreply.github.com>
    Co-authored-by: Professor Wayne B. Hayes, UC Irvine <whayes@uci.edu>

---
+++ regression-tests/blantSeed/test_blant_index.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+TEST_DIR=$(pwd)/regression-tests/blantSeed
+./blant -k8 -lDEG2 -sINDEX -mr -a1 networks/syeast0/syeast0.el >$TEST_DIR/syeast0.index
+./dedup.sh $TEST_DIR/syeast0.index
+
+DIFF=$(diff $TEST_DIR/syeast0.index examples/syeast0.index)
+
+if [ "$DIFF" == "" ]; then
+    echo "indexes identical"
+    exit 0
+else
+    echo "indexes different"
+    echo "diff is $DIFF"
+    exit 1
+fi
