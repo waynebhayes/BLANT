1944af16079c5e6b30815ded44a5cb6f6d2cacfb
waynebhayes@github.com <whayes@uci.edu>
2022-11-04 17:20:27 -0700

    reverse the sense of if/else

--- regression-tests/odvSeeds/test_odv_seeds.sh
+++ regression-tests/odvSeeds/test_odv_seeds.sh
@@ -4,6 +4,8 @@ TMPDIR=`mktemp -d /tmp/$BASENAME.XXXXXX`
 python3 seed_mining/odv_helpers.py networks/syeast0/syeast0.el networks/syeast05/syeast05.el seed_mining/examples/syeast0-k5.odv seed_mining/examples/syeast05-k5.odv 5 1004 >$TMPDIR/syeast0-syeast05-k5-n1004.odvseeds 2>/dev/null
 
 if diff $TMPDIR/syeast0-syeast05-k5-n1004.odvseeds seed_mining/examples/syeast0-syeast05-k5-n1004.odvseeds; then
+    :
+else
     echo "odv seeds different"
     exit 1
 fi
