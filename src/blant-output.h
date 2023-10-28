134a80b3ee26453d8b6e0ddd8a49d4ddc8cf0183
waynebhayes <whayes@uci.edu>
2023-10-28 16:25:31 -0700

    re-arrange PrintCanonical to allow PrintNonCanonical and PrintGraphletID

--- src/blant-output.h
+++ src/blant-output.h
@@ -7,14 +7,14 @@
 Boolean ProcessGraphlet(GRAPH *G, SET *V, unsigned Varray[], const int k, TINY_GRAPH *g, double weight);
 Boolean NodeSetSeenRecently(GRAPH *G, unsigned Varray[], int k);
 
-
 // None of the "Print" routines here actually print anything; they put the string into a constant internal buff, and then
 // YOU must print it (immediately is best, since the buffer will be over-written the next time you call the function).
-char *PrintCanonical(int GintOrdinal); // returns a string that YOU must print
+char *PrintGraphletID(Gint_type Gint); // returns a string that YOU must print
+char *PrintOrdinal(Gordinal_type GintOrdinal); // returns a string that YOU must print
 
 // For _outputMode = communityDetection, process the graphlet/orbit counts and neighbors
-void ProcessNodeOrbitNeighbors(GRAPH *G, Gint_type Gint, int GintOrdinal, unsigned Varray[], TINY_GRAPH *g, int k, double w);
-void ProcessNodeGraphletNeighbors(GRAPH *G, Gint_type Gint, int GintOrdinal, unsigned Varray[], TINY_GRAPH *g, int k, double w);
+void ProcessNodeOrbitNeighbors(GRAPH *G, Gint_type Gint, Gordinal_type GintOrdinal, unsigned Varray[], TINY_GRAPH *g, int k, double w);
+void ProcessNodeGraphletNeighbors(GRAPH *G, Gint_type Gint, Gordinal_type GintOrdinal, unsigned Varray[], TINY_GRAPH *g, int k, double w);
 
 // You should print a node EXCLUSIVELY with this function; it automatically determines if we're supporting names or not.
 // If c is non-zero, the character is prepended, using putchar(), before the node is printed (eg ' ', ';' or '\t').
@@ -22,7 +22,7 @@ char *PrintNode(char c, int v); // returns a pointer to a constant buffer; YOU n
 // Print a pair of nodes, but sorted so that u<=v, even if they are strings.
 char *PrintNodePairSorted(int u, char c, int v); // returns a pointer to a constant buffer, YOU must print it.
 
-char *PrintIndexEntry(Gint_type Gint, int GintOrdinal, unsigned Varray[], TINY_GRAPH *g, int k, double w);
-char *PrintIndexOrbitsEntry(Gint_type Gint, int GintOrdinal, unsigned Varray[], TINY_GRAPH *g, int k, double w);
+char *PrintIndexEntry(Gint_type Gint, Gordinal_type GintOrdinal, unsigned Varray[], TINY_GRAPH *g, int k, double w);
+char *PrintIndexOrbitsEntry(Gint_type Gint, Gordinal_type GintOrdinal, unsigned Varray[], TINY_GRAPH *g, int k, double w);
 
 #endif
