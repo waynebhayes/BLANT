ce53dd4ea822eff2eaeb1177c45730a3a3a09735
waynebhayes <whayes@uci.edu>
2023-10-30 10:54:21 -0700

    added MergeCluster but SegFault

--- src/ClusterScore/score.c
+++ src/ClusterScore/score.c
@@ -2,7 +2,7 @@
 #include "graph.h"
 #include "sets.h"
 #include "rand48.h"
-
+#include <math.h>
 
 typedef struct _clustering {
     GRAPH *G;
@@ -18,31 +18,35 @@ CLUSTERING *ClusteringAlloc(GRAPH *G) {
     C->clusters = Calloc(G->n, sizeof(SET*));
     C->clusters[0] = SetAlloc(G->n);
     for(int i=0;i < G->n; i++) SetAdd(C->clusters[0], i);
+    C->clusSize = Calloc(G->n, sizeof(unsigned));
     C->clusSize[0] = G->n;
     ++C->nC;
     return C;
 }
 
-double ScoreOneCluster(CLUSTERING *C, int who) {
-    unsigned nodes[C->G->n], n = SetToArray(nodes, C->clusters[who]), m=0;
+double ScoreOneCluster(GRAPH *G, SET *c) {
+    unsigned nodes[G->n], n = SetToArray(nodes, c), m=0;
     assert(n>0);
-    assert(n == C->clusSize[who]);
     if(n==1) return 0;
-    for(int i=0; i<n; i++) for(int j=i+1;j<n;j++) if(GraphAreConnected(C->G,nodes[i],nodes[j])) ++m;
+    for(int i=0; i<n; i++) for(int j=i+1;j<n;j++) if(GraphAreConnected(G,nodes[i],nodes[j])) ++m;
     double ED= m/(n*(n-1)/2.0);
     return m*ED;
 }
 
 double ScoreClustering(CLUSTERING *C) {
     double score=0;
-    for(int c=0; C->clusters[c]; c++) { double CS = ScoreOneCluster(C, c); printf(" %g", CS); score += CS; }
+    for(int c=0; C->clusters[c]; c++) {
+	double CS = ScoreOneCluster(C->G, C->clusters[c]); // printf(" %g", CS);
+	score += CS;
+    }
     return score;
 }
 
 
-// Remove some random set of elements from C[who], and return the set of removed elements
+// Remove some random set of elements from C[who], put them in C->clusters[nC], and increment C->nC
 void SplitCluster(CLUSTERING *C, unsigned who) {
     GRAPH *G=C->G;
+    assert(who < G->n);
     unsigned nodes[G->n], n = SetToArray(nodes, C->clusters[who]);
     assert(n>1);
     assert(C->clusters[C->nC]==NULL);
@@ -71,10 +75,10 @@ Boolean TrySplit(CLUSTERING *C){
 	oldSize = C->clusSize[who];
 	assert(++tries < G->n);
     }
-    double oldScore = ScoreOneCluster(C, who);
+    double oldScore = ScoreOneCluster(C->G, C->clusters[who]);
     SplitCluster(C, who);
     assert(C->nC == oldNC+1);
-    double newScore = ScoreOneCluster(C, who) + ScoreOneCluster(C, oldNC);
+    double newScore = ScoreOneCluster(C->G, C->clusters[who]) + ScoreOneCluster(C->G, C->clusters[oldNC]);
     if(newScore > oldScore) return true;
     else {
 	SetUnion(C->clusters[who], C->clusters[who], C->clusters[oldNC]);
@@ -87,13 +91,52 @@ Boolean TrySplit(CLUSTERING *C){
     }
 }
 
+Boolean TryMerge(CLUSTERING *C){
+    GRAPH *G = C->G;
+    assert(1 < C->nC && C->nC <= G->n);
+    assert(C->clusters[C->nC-1]);
+
+    int who[2]={-1,-1}, oldSize[2]={G->n,G->n};
+    for(int i=0;i<2;i++) {
+	int tries = 0;
+	while((who[i] = C->nC*drand48()) == who[1-i] ||
+	    // prefer to merge smaller clusters together instead of bigger ones
+	    C->clusSize[who[i]] > G->n*drand48()) assert(++tries < G->n);
+    }
+    assert(who[0] != who[1]);
+    assert(0 <= who[0] && who[0] < C->nC);
+    assert(0 <= who[1] && who[1] < C->nC);
+    double oldScore = ScoreOneCluster(C->G, C->clusters[who[0]]) + ScoreOneCluster(C->G, C->clusters[who[1]]);
+    static SET *merged;
+    if(merged) SetReset(merged); else merged = SetAlloc(C->G->n);
+    SetUnion(merged, C->clusters[who[0]], C->clusters[who[1]]);
+    double newScore = ScoreOneCluster(C->G, merged);
+    if(newScore < oldScore) return false;
+    else {
+	SetFree(C->clusters[who[0]]);
+	SetFree(C->clusters[who[1]]);
+	int keep;
+	if(who[0] < who[1]) keep=0; else keep=1;
+	C->clusters[who[keep]] = merged;
+	C->clusSize[who[keep]] = SetCardinality(merged);
+	merged = NULL;
+	C->clusters[who[1-keep]] = C->clusters[--C->nC];
+	C->clusters[C->nC] = NULL;
+	return true;
+    }
+}
+
 Boolean TryMove(CLUSTERING *C){
     assert(0 < C->nC && C->nC < C->G->n);
     assert(C->clusters[C->nC-1]);
     assert(C->clusters[C->nC]==NULL);
 
-    if(C->nC < C->G->n-1) return TrySplit(C);
-    return false;
+    // We want to bias towards a merge if there's "too many" clusters, and bias towards split if there's too few,
+    // but but we need at least 2 before we can merge.
+    double mergeProb = (C->nC-1.0)/C->G->n;
+    
+    if(drand48() < mergeProb) return TryMerge(C);
+    else return TrySplit(C);
 }
 
 int main(int argc, char *argv[])
@@ -104,14 +147,12 @@ int main(int argc, char *argv[])
 
     CLUSTERING *C = ClusteringAlloc(G);
 
-    // At this point we have one big cluster consisting of the entire graph, so start splitting
+    // At this point we have one big cluster consisting of the entire graph... split it once and get going....
+    SplitCluster(C, 0);
 
-    srand48(time(0));
     while(true) {
-	printf("Status: %d clusters: ", C->nC);
 	double fullScore = ScoreClustering(C);
-	printf(" full score %g\n", fullScore);
-	TryMove(C);
+	if(TryMove(C)) printf("Status: %d clusters: full score %g\n", C->nC, fullScore);
     }
     return 0;
 }
