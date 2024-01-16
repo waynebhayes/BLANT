b0b0990ee28dc549a8feca5a71e49212c147693a
waynebhayes@github.com <whayes@uci.edu>
2024-01-15 16:41:06 -0800

    new GDV regression test using absolute estimates

--- regression-tests/0-sanity/test2_GDV.sh
+++ regression-tests/0-sanity/test2_GDV.sh
@@ -1,34 +1,42 @@
 #!/bin/bash
 echo 'testing Graphlet (not orbit) Degree Vectors'
 
-# "correct" values came from 3e9 samples, so we divide by 1000 to correspond to testing with 3e6 samples
+# "correct" values came from 3e9 samples... but we no longer need to divide by 1000 since it's already absolute estimates
 
 N=3000000
-S=SEC
+
 export k=1
-for k in 3 4 5 6 7 8; do
-    echo "trying $k..."
-    if [ -f canon_maps/canon_map$k.bin ]; then
-	/bin/echo -n "$k: "
-	./blant -R -s $S -mg -n $N -k $k networks/syeast.el |
-	    sort -n | cut -d' ' -f2- |
-	    paste - <(unxz < regression-tests/0-sanity/syeast.$S.gdv.3e9.k$k.txt.xz) |
-	    awk 'function MIN(a,b){return (a<b)?a:b}
-		 function MAX(a,b){return (a>b)?a:b}
-		 {
-		    cols=NF/2;
-		    for(i=1;i<=cols;i++) if($i>1000 && $(cols+i)/1000>1000)
-			printf "%.9f\n", 1-MIN($i,$(cols+i)/1000)/MAX($i,$(cols+i)/1000)
-		}' |
-	    $LIBWAYNE_HOME/bin/stats | sed -e 's/#/num/' -e 's/var.*//' |
-	    $LIBWAYNE_HOME/bin/named-next-col '
-		{
-		    if(num<900 || mean>.005*'$k' || max>0.2 || stdDev>0.005*'$k'){
-			printf "BEYOND TOLERANCE:\n%s\n", $0;
-			exit 1
-		    } else
-			print $0
-		}' || break
-    fi
-done
-[ $k -eq 8 ] || exit 1
+for S in MCMC SEC; do
+    case $S in
+    MCMC) TOL=0.002; exp=2;;
+    SEC)  TOL=7e-5; exp=3;;
+    NBE)  TOL=0.003;;
+    esac
+
+    for k in 3 4 5 6 7 8
+    do
+	if [ -f canon_maps/canon_map$k.bin ]; then
+	    /bin/echo -n "$S:$k: "
+	    ./blant -R -s $S -mg -n $N -k $k networks/syeast.el |
+		sort -n | cut -d' ' -f2- |
+		paste - <(unxz < regression-tests/0-sanity/syeast.$S.gdv.abs.3e9.k$k.txt.xz) |
+		awk '{  cols=NF/2;
+			for(c1=1;c1<=cols;c1++){
+			    c2=cols+c1; if($c1&&$c2) print $c1/$c2
+			}
+		    }' |
+		$LIBWAYNE_HOME/bin/stats -g | # the -g option means "geometric mean"
+		sed -e 's/#/num/' -e 's/[	 ][	 ]*/ /g' |
+		$LIBWAYNE_HOME/bin/named-next-col '
+		    BEGIN{k='$k'}
+		    {
+			diff=ABS(1-mean)/(k*stdDev)^'$exp';
+			if(diff > '"$TOL"') {
+			    printf "BEYOND TOLERANCE: %g\n%s\n", diff, $0;
+			    exit 1
+			} else
+			    printf "diff %g\t%s\n", diff, $0;
+		    }' || exit 1
+	fi
+    done || exit 1
+done || exit 1
