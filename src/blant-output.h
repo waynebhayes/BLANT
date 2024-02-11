c3210879d4496caf6eb1fd46cfb4c6b555fbd876
waynebhayes <whayes@uci.edu>
2024-02-11 12:39:57 -0800

    added Simulated Annealing optimization of non-canonicals

--- src/blant-output.h
+++ src/blant-output.h
@@ -22,7 +22,7 @@ char *PrintNode(char c, int v); // returns a pointer to a constant buffer; YOU n
 // Print a pair of nodes, but sorted so that u<=v, even if they are strings.
 char *PrintNodePairSorted(int u, char c, int v); // returns a pointer to a constant buffer, YOU must print it.
 
-char *PrintIndexEntry(Gint_type Gint, Gordinal_type GintOrdinal, unsigned Varray[], TINY_GRAPH *g, int k, double w);
-char *PrintIndexOrbitsEntry(Gint_type Gint, Gordinal_type GintOrdinal, unsigned Varray[], TINY_GRAPH *g, int k, double w);
+char *PrintIndexEntry(Gint_type Gint, Gordinal_type GintOrdinal, unsigned Varray[], int k, double w);
+char *PrintIndexOrbitsEntry(Gint_type Gint, Gordinal_type GintOrdinal, unsigned Varray[], int k, double w);
 
 #endif
