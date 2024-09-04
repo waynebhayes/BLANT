3a958370d3823772b08bedccbc1f7cab066780e6
SMANahian <smanahian123@gmail.com>
2024-09-04 16:16:48 +0600

    Removed bad codes

--- src/blant-sampling.c
+++ src/blant-sampling.c
@@ -6,7 +6,6 @@
 #include "queue.h"
 #include "multisets.h"
 #include "blant-output.h"
-#include "what-the-graphlet.h"
 
 int _sampleMethod = -1, _sampleSubmethod = -1;
 FILE *_sampleFile; // if _sampleMethod is SAMPLE_FROM_FILE
@@ -282,33 +281,21 @@ double SampleGraphletNodeBasedExpansion(GRAPH *G, SET *V, unsigned *Varray, int 
 	    TinyGraphInducedFromGraph(T, G, Varray);
 	    Gint_type Gint = TinyGraph2Int(T, k);
 	    TinyGraphFree(T);
-	    Gordinal_type GintOrdinal;
+	    Gordinal_type GintOrdinal = L_K(Gint);
 
 	    double ocount = (double)multiplier/((double)_alphaList[GintOrdinal]);
-		unsigned char perm[k];
-		if(k <= 8) {
-			GintOrdinal = L_K(Gint);
-			memset(perm, 0, k);
-			ExtractPerm(perm, Gint);
-		} else {
-			Gint_type canon_value; //Change types
-			smaller_canon_map(Gint, k, &canon_value, perm);
-			for(j = 0; j < k; j++) {
-				if(perm[j] >= '0' && perm[j] <= '9') {
-					perm[j] = perm[j] - '0';
-				} else {
-					perm[j] = perm[j] - 'A' + 10;
-				}
-			}
-			GintOrdinal = canon_to_ordinal(canon_value, k);
-		}
-
 	    if (_outputMode == outputODV) {
+		unsigned char perm[k];
+		memset(perm, 0, k);
+		ExtractPerm(perm, Gint);
 		for (j = 0; j < k; j++) {
 		    _doubleOrbitDegreeVector[_orbitList[GintOrdinal][j]][Varray[(int)perm[j]]] += ocount;
 		}
 	    }
 		if (_outputMode == outputGDV) {
+			unsigned char perm[k];
+			memset(perm, 0, k);
+			ExtractPerm(perm, Gint);
 			for (j = 0; j < k; j++) {
 				_doubleGraphletDegreeVector[GintOrdinal][Varray[(int)perm[j]]] += ocount;
 			}
@@ -348,32 +335,20 @@ double SampleGraphletNodeBasedExpansion(GRAPH *G, SET *V, unsigned *Varray, int 
 	if(!g) g = TinyGraphAlloc(k);
 	TinyGraphInducedFromGraph(g, G, Varray);
 	Gint_type Gint = TinyGraph2Int(g, k);
-	Gordinal_type GintOrdinal;
-	unsigned char perm[k];
-	if(k <= 8) {
-		GintOrdinal = L_K(Gint);
-		memset(perm, 0, k);
-		ExtractPerm(perm, Gint);
-	} else {
-		Gint_type canon_value; //Change types
-		smaller_canon_map(Gint, k, &canon_value, perm);
-		for(j = 0; j < k; j++) {
-			if(perm[j] >= '0' && perm[j] <= '9') {
-				perm[j] = perm[j] - '0';
-			} else {
-				perm[j] = perm[j] - 'A' + 10;
-			}
-		}
-		GintOrdinal = canon_to_ordinal(canon_value, k);
-	}
+	Gordinal_type GintOrdinal = L_K(Gint);
 	double ocount = (double)multiplier/((double)_alphaList[GintOrdinal]);
-
 	if (_outputMode == outputODV) {
+	    unsigned char perm[k];
+	    memset(perm, 0, k);
+	    ExtractPerm(perm, Gint);
 	    for (j = 0; j < k; j++) {
 		_doubleOrbitDegreeVector[_orbitList[GintOrdinal][j]][Varray[(int)perm[j]]] += ocount;
 	    }
 	}
 	if (_outputMode == outputGDV) {
+		unsigned char perm[k];
+		memset(perm, 0, k);
+		ExtractPerm(perm, Gint);
 		for (j = 0; j < k; j++) {
 		    _doubleGraphletDegreeVector[GintOrdinal][Varray[(int)perm[j]]] += ocount;
 		}
@@ -714,32 +689,20 @@ double SampleGraphletEdgeBasedExpansion(GRAPH *G, SET *V, unsigned *Varray, int 
 	TINY_GRAPH *g = TinyGraphAlloc(k);
 	TinyGraphInducedFromGraph(g, G, Varray);
 	Gint_type Gint = TinyGraph2Int(g, k);
-	Gordinal_type GintOrdinal;
-	unsigned char perm[k];
-	if(k <= 8) {
-		GintOrdinal = L_K(Gint);
-		memset(perm, 0, k);
-		ExtractPerm(perm, Gint);
-	} else {
-		Gint_type canon_value; //Change types
-		smaller_canon_map(Gint, k, &canon_value, perm);
-		for(j = 0; j < k; j++) {
-			if(perm[j] >= '0' && perm[j] <= '9') {
-				perm[j] = perm[j] - '0';
-			} else {
-				perm[j] = perm[j] - 'A' + 10;
-			}
-		}
-		GintOrdinal = canon_to_ordinal(canon_value, k);
-	}
+	Gordinal_type GintOrdinal = L_K(Gint);
 	double ocount = (double)multiplier/((double)_alphaList[GintOrdinal]);
-
 	if (_outputMode == outputODV) {
+	    unsigned char perm[k];
+	    memset(perm, 0, k);
+	    ExtractPerm(perm, Gint);
 	    for (j = 0; j < k; j++) {
 		_doubleOrbitDegreeVector[_orbitList[GintOrdinal][j]][Varray[(int)perm[j]]] += ocount;
 	    }
 	}
 	if (_outputMode == outputGDV) {
+		unsigned char perm[k];
+		memset(perm, 0, k);
+		ExtractPerm(perm, Gint);
 		for (j = 0; j < k; j++) {
 		    _doubleGraphletDegreeVector[GintOrdinal][Varray[(int)perm[j]]] += ocount;
 		}
@@ -988,25 +951,7 @@ double SampleGraphletMCMC(GRAPH *G, SET *V, unsigned *Varray, int k, int whichCC
     }
     TinyGraphInducedFromGraph(g, G, Varray);
     Gint_type Gint = TinyGraph2Int(g, k);
-    Gordinal_type GintOrdinal;
-	unsigned char perm[k];
-	if(k <= 8) {
-		GintOrdinal = L_K(Gint);
-		memset(perm, 0, k);
-		ExtractPerm(perm, Gint);
-	} else {
-		Gint_type canon_value; //Change types
-		smaller_canon_map(Gint, k, &canon_value, perm);
-		for(j = 0; j < k; j++) {
-			if(perm[j] >= '0' && perm[j] <= '9') {
-				perm[j] = perm[j] - '0';
-			} else {
-				perm[j] = perm[j] - 'A' + 10;
-			}
-		}
-		GintOrdinal = canon_to_ordinal(canon_value, k);
-	}
-
+    Gordinal_type GintOrdinal = L_K(Gint);
 
     assert(numNodes == k); // Ensure we are returning k nodes
     Boolean found=false;
@@ -1029,11 +974,17 @@ double SampleGraphletMCMC(GRAPH *G, SET *V, unsigned *Varray, int k, int whichCC
 	ocount = (double)multiplier/((double)_alphaList[GintOrdinal]);
     }
     if (_outputMode == outputODV) {
+	unsigned char perm[k];
+	memset(perm, 0, k);
+	ExtractPerm(perm, Gint);
 	for (j = 0; j < k; j++) {
 	    _doubleOrbitDegreeVector[_orbitList[GintOrdinal][j]][Varray[(int)perm[j]]] += ocount;
 	}
     }
 	if (_outputMode == outputGDV) {
+		unsigned char perm[k];
+		memset(perm, 0, k);
+		ExtractPerm(perm, Gint);
 		for (j = 0; j < k; j++) {
 		    _doubleGraphletDegreeVector[GintOrdinal][Varray[(int)perm[j]]] += ocount;
 		}
@@ -1163,24 +1114,7 @@ double SampleGraphletSequentialEdgeChaining(GRAPH *G, SET *V, unsigned *Varray, 
 
     TinyGraphInducedFromGraph(g, G, Varray);
     Gint_type Gint = TinyGraph2Int(g, k);
-    Gordinal_type GintOrdinal;
-	unsigned char perm[k];
-	if(k <= 8) {
-		GintOrdinal = L_K(Gint);
-		memset(perm, 0, k);
-		ExtractPerm(perm, Gint);
-	} else {
-		Gint_type canon_value; //Change types
-		smaller_canon_map(Gint, k, &canon_value, perm);
-		for(j = 0; j < k; j++) {
-			if(perm[j] >= '0' && perm[j] <= '9') {
-				perm[j] = perm[j] - '0';
-			} else {
-				perm[j] = perm[j] - 'A' + 10;
-			}
-		}
-		GintOrdinal = canon_to_ordinal(canon_value, k);
-	}
+    Gordinal_type GintOrdinal = L_K(Gint);
 
     double ocount = 1.0;
 
@@ -1188,11 +1122,17 @@ double SampleGraphletSequentialEdgeChaining(GRAPH *G, SET *V, unsigned *Varray, 
     ocount = (double)multiplier/((double)_alphaList[GintOrdinal]);
 
     if (_outputMode == outputODV) {
+	unsigned char perm[k];
+	memset(perm, 0, k);
+	ExtractPerm(perm, Gint);
 	for (j = 0; j < k; j++) {
 	    _doubleOrbitDegreeVector[_orbitList[GintOrdinal][j]][Varray[(int)perm[j]]] += ocount;
 	}
     }
     if (_outputMode == outputGDV) {
+	unsigned char perm[k];
+	memset(perm, 0, k);
+	ExtractPerm(perm, Gint);
 	for (j = 0; j < k; j++) {
 	    _doubleGraphletDegreeVector[GintOrdinal][Varray[(int)perm[j]]] += ocount;
 	}
