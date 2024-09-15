5ab39e8573aa7ef10ba58291cff50a2272e9bd03
Wayne Hayes <whayes@uci.edu>
2024-09-15 13:28:11 -0700

    set MAX_K back to 8 by default

--- src/blant-predict-stub.c
+++ src/blant-predict-stub.c
@@ -6,8 +6,10 @@
 void Predict_Init(GRAPH *G){Fatal("prediction not supported");}
 void Predict_FlushMotifs(GRAPH *G){Fatal("prediction not supported");};
 void Predict_ProcessLine(GRAPH *G, char line[]){Fatal("prediction not supported");};
-unsigned int L_K_Func(Gint_type Gint) {Apology("prediction not supported"); return -1;}
-
-void Predict_AccumulateMotifs(GRAPH *G, unsigned Varray[], TINY_GRAPH *g, Gint_type Gint, Gordinal_type GintOrdinal){Fatal("prediction not supported by your version of gcc");};
-int  Predict_Merge(GRAPH *G){Fatal("prediction not supported by your version of gcc"); return -1;}
-
+Gordinal_type L_K_Func(Gint_type Gint) {Apology("prediction not supported"); return -1;}
+int  Predict_Merge(GRAPH *G){Fatal("prediction not supported"); return -1;}
+Gordinal_type canon_to_ordinal(Gint_type canon, int k){Fatal("prediction not supported"); return -1;}
+void Predict_AccumulateMotifs(GRAPH *G, unsigned Varray[], TINY_GRAPH *g, Gint_type Gint, Gordinal_type GintOrdinal)
+    {Fatal("prediction not supported");};
+Gint_type smaller_canon_map(Gint_type num, int k, unsigned char* return_permutation)
+    {Fatal("prediction not supported"); return -1;}
