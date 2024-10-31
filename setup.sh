6b499e7ece28cbc44f1ed2fa33b071e9146a2eb3
waynebhayes@github.com <whayes@uci.edu>
2024-10-31 13:27:48 -0700

    fixed setup.sh by removing hayes-specific stuff

--- setup.sh
+++ setup.sh
@@ -12,13 +12,8 @@ not(){ if eval "$@"; then return 1; else return 0; fi; }
 newlines(){ awk '{for(i=1; i<=NF;i++)print $i}' "$@"; }
 parse(){ awk "BEGIN{print $*}" </dev/null; }
 which(){ echo "$PATH" | tr : "$NL" | awk '!seen[$0]{print}{++seen[$0]}' | while read d; do eval /bin/ls $d/$N; done 2>/dev/null | newlines; }
-
-# Temporary Filename + Directory (both, you can use either, note they'll have different random stuff in the XXXXXX part)
-BIGTMP=`for i in /scratch/preserve/RaidZ3/tmp /var/tmp /scratch/preserve /var/tmp /tmp; do mkdir -p "$i/wayne" && (df $i | awk 'NR==1{for(av=1;av<=NF;av++)if(match($av,"[Aa]vail"))break;}NR>1{print $av,"'"$i"'"}'); done 2>/dev/null | sort -nr | awk 'NR==1{print $2}'`
-[ "$MYTMP" ] || MYTMP="$BIGTMP/wayne"
-TMPDIR=`mktemp -d $MYTMP/$BASENAME.XXXXXX`
- trap "/bin/rm -rf $TMPDIR; exit" 0 1 2 3 15 # call trap "" N to remove the trap for signal N
-
+export TAB='	'; export NL='
+'
 #################### END OF SKELETON, ADD YOUR CODE BELOW THIS LINE
 
 # if SETUP_USAGE have set, exit
@@ -38,4 +33,6 @@ if [ "$BLANT_HOME" = . ]; then BLANT_HOME=`/bin/pwd`; fi
 [ -d libwayne/bin ] || die "need libwayne/bin directory, please clone libwayne while in $BLANT_HOME"
 [ -x libwayne/bin/hawk ] || die "no hawk in libwayne/bin???"
 
-export PATH="$BLANT_HOME/libwayne/bin:$BLANT_HOME/scripts:$PATH"
+echo Setting PATH appropriately...
+# the back-quote command removes duplicate directories in the PATH
+export PATH=`echo "$BLANT_HOME/libwayne/bin:$BLANT_HOME/scripts:$PATH" | tr : "$NL" | awk '!seen[$0]{print}{++seen[$0]}' | tr "$NL" :`
