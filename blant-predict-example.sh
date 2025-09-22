94162fde84fa995f02ae9692245142be373fdd0d
waynebhayes@github.com <whayes@uci.edu>
2025-09-22 13:58:23 -0700

    set PATH and LIBWAYNE_HOME in blant-predict-example.sh

--- blant-predict-example.sh
+++ blant-predict-example.sh
@@ -4,16 +4,16 @@
 if [ $# -ne 1 ]; then echo "must provide number of predictions as first argument" >&2; exit 1; fi
 
 TOP="$1" # number of predictions to take
-
 LIBWAYNE_HOME=`cd libwayne && /bin/pwd`
-export LIBWAYNE_HOME
+PATH="$PATH:$LIBWAYNE_HOME/bin:$LIBWAYNE_HOME/../scripts"
+export LIBWAYNE_HOME PATH
 # compile the executable
 [ -f libwayne/libwayne.a ] || (cd libwayne && make all)
-./libwayne/bin/wgcc -O3 -Isrc -o blant-predict-release src/blant-predict-release.c
+wgcc -O3 -Isrc -o blant-predict-release src/blant-predict-release.c
 
 # create a random-order list of HI-union edges
 TMP=/tmp/HI-union
-./scripts/randomizeLines < networks/HI-union.el > $TMP.el
+randomizeLines < networks/HI-union.el > $TMP.el
 
 # separate into 90% train, 10% test
 FOLD=`wc -l < $TMP.el` # number of edges in network
@@ -26,7 +26,7 @@ tail -n +$FOLD1 $TMP.el > $TMP.train
 ./blant-predict-release -k4 -mp6,0,0 $TMP.train | # estimate L3-paths on 4-node graphlets as in Kovacs et al.
     sort -gr | # sort the output node-pairs by L3 count, highest-to-lowest
     cut -f2 | # extract the node-pairs
-    ./libwayne/bin/hawk '{OFS="\t"; print MAX($1,$2), MIN($1,$2)}' | # print the tab-separated node-pairs in correct direction
+    hawk '{OFS="\t"; print MAX($1,$2), MIN($1,$2)}' | # print the tab-separated node-pairs in correct direction
     fgrep -vf $TMP.train | # remove the edges that are in the training set
     head -$TOP | # take the top 1,000 predicted edges
     fgrep -xcf - $TMP.test # count the correct
