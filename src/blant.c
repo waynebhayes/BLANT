1af28ddd9a6a12e5e62966fe8c4de923d1f9d20d
alanl <alanlin9535@gmail.com>
2026-07-21 02:17:33 +0100

    Skip if no valid CCs

--- src/blant.c
+++ src/blant.c
@@ -269,7 +269,7 @@ static int InitializeConnectedComponents(GRAPH *G) {
     _combinations[i] = CombinChooseDouble(_componentSize[i], _k);
     _totalCombinations += _combinations[i];
   }
-
+  if(_totalCombinations == 0) Fatal("Cannot sample - all components are smaller than k=%d", _k);
   double cumulativeProb = 0.0;
   for (i = 0; i < _numConnectedComponents; i++) {
     _probOfComponent[i] = _combinations[i] / _totalCombinations;
